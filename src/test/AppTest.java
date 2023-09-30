68bdc46fd76d8edc08007a5e55a624c226218dd3
Loan Tremoulet Breton <loan.tremoulet-breton01@etu.umontpellier.fr>
2023-09-30 18:50:48 +0200

    Mise en place du corps de l'application

---
+++ src/test/AppTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class AppTest {
+    @Test
+    public void test_if_main_method_was_implemented() {
+        assertDoesNotThrow(() -> App.main(new String[1]));
+    }
+
+}
