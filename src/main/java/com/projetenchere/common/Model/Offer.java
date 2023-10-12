15f7f70f885767dc285a65ed151bad6cf1f77b9e
Paul Jacquemin <paul.jacquemin@umontpellier.fr>
2023-10-12 16:06:29 +0200

    Ajouté le contenu de la classe offer

--- src/main/java/com/projetenchere/common/Model/Offer.java
+++ src/main/java/com/projetenchere/common/Model/Offer.java
@@ -1,4 +1,13 @@
 package com.projetenchere.common.Model;
 
+import java.text.DecimalFormat;
+
 public class Offer {
+    private final float value;
+
+    public Offer(String value) throws NumberFormatException{
+        DecimalFormat df = new DecimalFormat();
+        df.setMaximumFractionDigits(2);
+        this.value = Float.parseFloat(df.format(value));
+    }
 }
