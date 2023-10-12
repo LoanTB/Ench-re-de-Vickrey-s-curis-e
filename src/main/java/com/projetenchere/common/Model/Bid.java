60a6687ef67ad2352b358e8284894ae8b4d40b8a
Paul Jacquemin <paul.jacquemin@umontpellier.fr>
2023-10-12 16:29:23 +0200

    Renommé offre en bid

--- src/main/java/com/projetenchere/common/Model/Bid.java
+++ src/main/java/com/projetenchere/common/Model/Bid.java
@@ -2,10 +2,10 @@ package com.projetenchere.common.Model;
 
 import java.text.DecimalFormat;
 
-public class Offer {
+public class Bid {
     private final float value;
 
-    public Offer(String value) throws NumberFormatException{
+    public Bid(String value) throws NumberFormatException{
         DecimalFormat df = new DecimalFormat();
         df.setMaximumFractionDigits(2);
         this.value = Float.parseFloat(df.format(value));
