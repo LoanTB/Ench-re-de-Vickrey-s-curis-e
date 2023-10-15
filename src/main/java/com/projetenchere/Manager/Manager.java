512b157a18065b53e2b522657187030cad450791
auxilienk <auxilien@etud.fr>
2023-10-15 13:20:49 +0200

    Ajout des fonctions pour concernant le traitement des prix par le gestionnaire, et la confirmation via le terminal.

--- src/main/java/com/projetenchere/Manager/Manager.java
+++ src/main/java/com/projetenchere/Manager/Manager.java
@@ -1,5 +1,91 @@
 package com.projetenchere.Manager;
 
+/*--- Java libraries ---*/
+import java.io.*;
+import java.util.*;
+
 class Manager {
+
+/*------ Auction launch ------*/
+    //Creer l'enchère.
+
+    //Lancer l'enchère donc l'envoyer aux Bidders + envoyer la clé publique.
+
+/*------ Offer received ------*/
+
+    //Recevoir les offres.
+
+/*------ Price processing ------*/
+
+    //Déchiffrer les offres.
+
+    /**
+     *
+     * @return boolean, false if the manager say "n" and true if it say "y".
+     */
+    public static boolean checkWinner()
+    {
+        String confirmation = "";
+        boolean waitConfirmation = true;
+        Scanner scan = new Scanner(System.in);
+        while(waitConfirmation)
+        {
+            System.out.println("Est-ce correct ? y/n");
+            confirmation = scan.nextLine();
+            confirmation = confirmation.replaceAll("\n","");
+            confirmation = confirmation.toLowerCase();
+
+            if (confirmation.startsWith("y"))
+            {
+                waitConfirmation=false;
+                System.out.println("Ok.");
+                return true;
+            }
+            if (confirmation.startsWith("n"))
+            {
+                waitConfirmation=false;
+                System.out.println("Resort...");
+                return false;
+            }
+        }
+        return false;
+    }
+
+    /**
+     *
+     * @param prices to sort.
+     * @return winner price.
+     */
+    private static int getWinnerPrice(int[] prices)
+    {
+        System.out.println("Original : ");
+        for(int num : prices)
+        {
+            System.out.println(num + "");
+        }
+        Arrays.sort(prices); //Tri du plus petit au plus grand.
+        System.out.println("Sorted : ");
+
+        for(int num : prices)
+        {
+            System.out.println(num + "");
+        }
+
+        return prices[prices.length -2];
+    }
+
+    /**
+     *
+     * @param BidderPrice
+     */
+    public static void priceProcessing(int[] BidderPrice)
+    {
+        boolean correct = false;
+        while(!correct) {
+            int winnerPrice = getWinnerPrice(BidderPrice);
+            System.out.println("Winner price is : " + winnerPrice);
+            correct = checkWinner();
+        }
+    }
 
 }
--- src/main/java/com/projetenchere/Manager/ManagerApp.java
+++ src/main/java/com/projetenchere/Manager/ManagerApp.java
@@ -1,6 +1,21 @@
 package com.projetenchere.Manager;
 
 public class ManagerApp {
-    public static void main(String[] args) {System.out.println("Hello World");}
+    public static void main(String[] args) {
+        System.out.println("Bienvenue Manager !");
+
+        //Lancer enchère.
+
+        //Recevoir les prix.
+
+        //Traiter les prix.
+
+        //Manager.priceProcessing(BidderPrice);
+
+        //? Fin ?
+
+
+
+    }
 }
 
