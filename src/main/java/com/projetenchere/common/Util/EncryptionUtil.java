42d6f32a28f14a25586318415767b61321e43e78
Yûki <kilakiyu+java@gmail.com>
2023-10-15 21:50:48 +0200

    Push & Commit du fichier Encryption Util. Fonctions statiques permettant de chiffrer et déchiffrer des chaines de caractères.

--- src/main/java/com/projetenchere/common/Util/EncryptionUtil.java
+++ src/main/java/com/projetenchere/common/Util/EncryptionUtil.java
@@ -1,4 +1,31 @@
 package com.projetenchere.common.Util;
-
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Security;
+import java.security.Cipher;
+// Nous allons utiliser RSA
 public class EncryptionUtil {
+    // Générer une clé pair et la return
+    public static KeyPair generateKeyPair() throws Exception {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048); // Taille de la clé
+        return keyPairGenerator.generateKeyPair();
+    }
+
+    // Chiffrer un string en utilisant RSA et une clé publique
+    public static byte[] encrypt(String text, PublicKey publicKey) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA");  
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        return cipher.doFinal(text.getBytes());
+    }
+
+    // Déchiffrer un string en utiliser RSA et une clé privée
+    public static String decrypt(byte[] ciphertext, PrivateKey privateKey) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        byte[] decryptedBytes = cipher.doFinal(ciphertext);
+        return new String(decryptedBytes);
+    }
 }
