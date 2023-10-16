dceb4b5703f4e67eaff0e69a6dafc5ced5f557bc
auxilienk <auxilien@etud.fr>
2023-10-16 17:53:00 +0200

    Ajout des fonctions de mélange Seller.shuffleCypher de lien entre chiffré et place de l'enchérisseur dans l'odre de reception des chiffrés Seller.getBidderWinner

--- src/main/java/com/projetenchere/Seller/Seller.java
+++ src/main/java/com/projetenchere/Seller/Seller.java
@@ -1,5 +1,50 @@
 package com.projetenchere.Seller;
 
+/*------ Libraries ------*/
+import java.util.Arrays;
+import java.util.Random;
+
 class Seller {
+
+    /**
+     * @param prices_cypher le tableau des prix chiffrés.
+     * @return Le tableau prices_cypher mélangé.
+     */
+    public static int[] shuffleCypher(int prices_cypher[])
+    {
+        int[] prices_cypher_shuffled = prices_cypher.clone();
+        System.out.println(Arrays.toString(prices_cypher_shuffled));
+        Random rand = new Random();
+
+        for (int i = 0; i < prices_cypher_shuffled.length ; i ++)
+        {
+            int randomIndexToSwap = rand.nextInt(prices_cypher_shuffled.length);
+            int temp = prices_cypher_shuffled[randomIndexToSwap];
+            prices_cypher_shuffled[randomIndexToSwap] = prices_cypher_shuffled[i];
+            prices_cypher_shuffled[i] = temp;
+        }
+        System.out.println(Arrays.toString(prices_cypher_shuffled));
+
+        return prices_cypher_shuffled;
+    }
+
+    /**
+     * @param prices_cypher le tableau de prix chiffrés.
+     * @param winner_price_and_cypher le tableau avec index 0 le chiffré et index 1 le prix.
+     * @return la place de l'enchérisseur gagnant dans l'ordre de reception des prix.
+     */
+    public static int getBidderWinner(int[] prices_cypher,int[] winner_price_and_cypher)
+    {
+        int bidder_winner_reveived_place = 0;
+        for (int i=0; i < prices_cypher.length ; i++)
+        {
+            if(prices_cypher[i] == winner_price_and_cypher[0])
+            {
+                bidder_winner_reveived_place = i;
+                return bidder_winner_reveived_place;
+            }
+        }
+        return bidder_winner_reveived_place;
+    }
 
 }
--- src/main/java/com/projetenchere/Seller/SellerApp.java
+++ src/main/java/com/projetenchere/Seller/SellerApp.java
@@ -1,6 +1,30 @@
 package com.projetenchere.Seller;
 
 public class SellerApp {
-    public static void main(String[] args) {System.out.println("Hello World");}
+
+    public static void main(String[] args)
+    {
+        System.out.println("Bienvenue vendeur !");
+
+        //Recevoir les prix chiffrés.
+        //int[] prices_cypher = ;
+
+        //Mélanger les prix chiffrés.
+        //int[] prices_cypher_shuffled = Seller.shuffleCypher(prices_cypher);
+
+        //Envoyer les prix chiffrés au gestionnaire
+
+        //Recevoir le prix gagnant
+        //int winner_price_and_cypher[] = (prices_cypher_shuffled);
+
+        //Relier le chiffré gagnant au tableau de prix reçu,
+        // afin de connaitre la place du gagnant dans l'odre de reception.
+
+
+        //int bidder_winner_reveived_place = Seller.getBidderWinner(prices_cypher,winner_price_and_cypher);
+
+        //Envoyer aux enchérisseurs leurs résultats.
+
+    }
 }
 
