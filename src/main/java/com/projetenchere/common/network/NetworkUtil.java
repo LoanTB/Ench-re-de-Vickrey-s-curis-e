0109489ddf41c17c140d6526a94cbe1565d7e646
Loan Tremoulet Breton <loan.tremoulet-breton01@etu.umontpellier.fr>
2023-10-19 19:07:40 +0200

    Mise en place de sécurité réseau pour obliger le ObjectSender en type de transfert.

--- src/main/java/com/projetenchere/common/network/NetworkUtil.java
+++ src/main/java/com/projetenchere/common/network/NetworkUtil.java
@@ -1,6 +1,7 @@
 package com.projetenchere.common.network;
 
 import java.io.IOException;
+import java.io.InvalidClassException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.InetAddress;
@@ -10,19 +11,23 @@ import java.net.UnknownHostException;
 
 public class NetworkUtil {
 
-    public static Object receive(int port) throws IOException, ClassNotFoundException {
+    public static ObjectSender receive(int port) throws IOException, ClassNotFoundException, InvalidClassException {
         try (ServerSocket serverSocket = new ServerSocket(port);
              Socket clientSocket = serverSocket.accept();
              ObjectInputStream objectInput = new ObjectInputStream(clientSocket.getInputStream())) {
 
-            return objectInput.readObject();
+            Object object = objectInput.readObject();
+            if (object instanceof ObjectSender) {
+                return (ObjectSender) object;
+            } else {
+                throw new InvalidClassException("Received object is not an instance of ObjectSender");
+            }
         }
     }
 
     public static void send(String serverAddress, int serverPort, Object data) throws IOException {
         try (Socket clientSocket = new Socket(serverAddress, serverPort);
              ObjectOutputStream objectOutput = new ObjectOutputStream(clientSocket.getOutputStream())) {
-
             objectOutput.writeObject(data);
             objectOutput.flush();
         }
--- src/test/java/com/projetenchere/common/NetworkUtilTest.java
+++ src/test/java/com/projetenchere/common/NetworkUtilTest.java
@@ -13,36 +13,6 @@ public class NetworkUtilTest {
     Thread clientThread;
 
     @Test
-    public void test_basic_string_transmission() throws InterruptedException {
-        final String[] data = new String[2]; // Pour avoir de la mémoire partagé avec les threads
-
-        serverThread = new Thread(() -> { // Thread serveur
-            try {
-                data[0] =  new String("Je suis un test");
-                NetworkUtil.send(NetworkUtil.getMyIP(), 24681,data[0]);
-            } catch (IOException e) {
-                throw new RuntimeException("Erreur côté serveur: " + e);
-            }
-        });
-
-        clientThread = new Thread(() -> { // Thread client
-            try {
-                data[1] = (String) NetworkUtil.receive(24681);
-            } catch (IOException | ClassNotFoundException e) {
-                throw new RuntimeException("Erreur côté client: " + e);
-            }
-        });
-
-        serverThread.start(); // Démarrer le serveur
-        clientThread.start(); // Démarrer le client
-
-        serverThread.join(); // Attendre serveur
-        clientThread.join(); // Attendre client
-
-        assertEquals(data[0], data[1]);
-    }
-
-    @Test
     public void test_ObjectSender_transmission() throws InterruptedException {
         final ObjectSender[] data = new ObjectSender[2];
 
