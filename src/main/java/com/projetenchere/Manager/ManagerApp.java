14b860054e4ba464c941434014269cd3d845671c
auxilienk <auxilien@etud.fr>
2023-10-19 18:52:25 +0200

    Ajout des appels de méthodes de chiffrement dans le main. Création d'un BidStarter (qui contient l'enchère Bid et la clé publique)

--- src/main/java/com/projetenchere/Manager/ManagerApp.java
+++ src/main/java/com/projetenchere/Manager/ManagerApp.java
@@ -1,11 +1,13 @@
 package com.projetenchere.Manager;
 
 import com.projetenchere.Manager.Controller.ManagerController;
+import com.projetenchere.Manager.Controller.ManagerNetworkController;
+import com.projetenchere.common.Model.Bid;
 
 import java.security.KeyPair;
 import java.security.PrivateKey;
 import java.security.PublicKey;
-import java.util.Base64;
+
 
 public class ManagerApp {
     public static void main(String[] args) throws Exception {
@@ -14,13 +16,21 @@ public class ManagerApp {
         ManagerController controller = new ManagerController();
         //Lancer enchère.
 
+        Bid currentBid = controller.createBid();
         KeyPair ManagerKeys = controller.generateManagerKeys();
-
 
         PrivateKey managerPrivateKey = ManagerKeys.getPrivate();
         PublicKey managerPublicKey = ManagerKeys.getPublic();
 
+        //TODO : Recevoir requête Enchérisseur.
+
+        ManagerNetworkController networkController = new ManagerNetworkController();
+
+
+
         //Recevoir les prix.
+
+        //controller.fetchEncryptedOffers();
 
         //Traiter les prix.
 
