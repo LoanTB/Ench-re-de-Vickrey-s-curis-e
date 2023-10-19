c796ab623db9385cd7faa5d4df4c221e015d7bbf
auxilienk <auxilien@etud.fr>
2023-10-19 18:48:03 +0200

    Update Manager : Nouveau controleur dédié au Manager.

--- src/main/java/com/projetenchere/Manager/Controller/ManagerNetworkController.java
+++ src/main/java/com/projetenchere/Manager/Controller/ManagerNetworkController.java
@@ -1,6 +1,31 @@
 package com.projetenchere.Manager.Controller;
 
 import com.projetenchere.common.network.NetworkUtil;
+import com.projetenchere.common.network.ObjectSender;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class ManagerNetworkController {
+    private String ManagerIp;
+    private int ManagerPort = 2468;
+
+    public String getManagerIp() {
+        return ManagerIp;
+    }
+
+    public void setManagerIp(String managerIp) {
+        ManagerIp = managerIp;
+    }
+
+    public ManagerNetworkController() throws UnknownHostException {
+        String ManagerIp = NetworkUtil.getMyIP();
+        setManagerIp(ManagerIp);
+    }
+
+    /*
+    public ObjectSender createObjectSender() {
+        //TODO :
+    }
+    */
 }
