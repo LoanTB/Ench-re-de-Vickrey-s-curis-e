8e6cf8fdfa0f280b8dac4877a647b69973e58f8a
Loan Tremoulet Breton <loan.tremoulet-breton01@etu.umontpellier.fr>
2023-10-19 08:21:28 +0200

    Clarification et quelques améliorations du code placé dans NetworkUtil. et création de reel test.

--- src/main/java/com/projetenchere/common/Util/NetworkUtil.java
+++ src/main/java/com/projetenchere/common/Util/NetworkUtil.java
@@ -1,4 +1,44 @@
 package com.projetenchere.common.Util;
 
+import java.io.*;
+import java.net.*;
+
 public class NetworkUtil {
+
+    public static <T> T recevoirObjet(int port, Class<T> expectedClass) throws IOException, ClassNotFoundException {
+        try (ServerSocket serverSocket = new ServerSocket(port);
+             Socket clientSocket = serverSocket.accept();
+             ObjectInputStream objectInput = new ObjectInputStream(clientSocket.getInputStream())) {
+
+            // Récupération du nom de la classe
+            String className_RecupClient = (String) objectInput.readObject();
+
+            // Vérification du nom de la classe
+            if (!className_RecupClient.equals(expectedClass.getName())) {
+                throw new IllegalArgumentException("Le type d'objet reçu est différent du type attendu");
+            }
+
+            // Récupération de l'objet
+            return expectedClass.cast(objectInput.readObject());
+        }
+    }
+
+    public static void envoiObjet(String serverAddress, int serverPort, Object objetEnvoi) throws IOException {
+        try (Socket clientSocket = new Socket(serverAddress, serverPort);
+             ObjectOutputStream objectOutput = new ObjectOutputStream(clientSocket.getOutputStream())) {
+
+            // Envoi du type de l'objet
+            objectOutput.writeObject(objetEnvoi.getClass().getName());
+            objectOutput.flush();
+
+            // Envoi de l'objet
+            objectOutput.writeObject(objetEnvoi);
+            objectOutput.flush();
+        }
+    }
+
+    public static String getMyIP() throws UnknownHostException {
+        InetAddress localHost = InetAddress.getLocalHost();
+        return localHost.getHostAddress();
+    }
 }
--- src/test/java/com/projetenchere/Util/NetworkUtilTest.java
+++ src/test/java/com/projetenchere/Util/NetworkUtilTest.java
@@ -1,4 +1,43 @@
 package com.projetenchere.Util;
 
+import com.projetenchere.common.Util.NetworkUtil;
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class NetworkUtilTest {
+
+    Thread serverThread;
+    Thread clientThread;
+
+    @Test
+    public void test_basic_string_transmission() throws InterruptedException {
+        final String[] data = new String[2];
+
+        serverThread = new Thread(() -> {
+            try {
+                data[0] =  new String("Je suis un test");
+                NetworkUtil.envoiObjet(NetworkUtil.getMyIP(), 24681,data[0]);
+            } catch (IOException e) {
+                throw new RuntimeException("Erreur côté serveur: " + e);
+            }
+        });
+
+        clientThread = new Thread(() -> {
+            try {
+                data[1] = NetworkUtil.recevoirObjet(24681,String.class);
+            } catch (IOException | ClassNotFoundException e) {
+                throw new RuntimeException("Erreur côté client: " + e);
+            }
+        });
+
+        serverThread.start(); // Démarrer le serveur
+        clientThread.start(); // Démarrer le client
+
+        serverThread.join(); // Attendre serveur
+        clientThread.join(); // Attendre client
+
+        assertEquals(data[0], data[1]);
+    }
 }
