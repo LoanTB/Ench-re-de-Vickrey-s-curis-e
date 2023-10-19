9dc8172512a5442f67fd4250663f87de6ff991d5
auxilienk <auxilien@etud.fr>
2023-10-19 16:52:24 +0200

    Construction de Bid (getter, setter, contructeur).

--- src/main/java/com/projetenchere/common/Model/Bid.java
+++ src/main/java/com/projetenchere/common/Model/Bid.java
@@ -1,12 +1,38 @@
 package com.projetenchere.common.Model;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Bid {
+    private String BidName;
+    private String BidDescription;
+    private LocalDate date;
+    public void Bid(String name, String description)
+    {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        date=LocalDate.from(localDateTime);
+        BidDescription=description;
+        BidName=name;
+    }
+
+    public String getBidName() {
+        return BidName;
+    }
+
+    public String getBidDescription() {
+        return BidDescription;
+    }
+
+    public LocalDate getDate(){
+        return date;
+    }
 
     public String _toString() {
-        return "Placeholder Bid";
+        return "Nom : "+ BidName + " Date : " + date.toString() +" Description : " + BidDescription + ".";
     }
 
     public boolean isOver() {
         return true;
     }
+
 }
