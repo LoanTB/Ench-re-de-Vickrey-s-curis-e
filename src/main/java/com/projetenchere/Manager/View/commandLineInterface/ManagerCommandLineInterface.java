7ddcd6fa961d1257d98acc29c2a84818ef5ada03
auxilienk <auxilien@etud.fr>
2023-10-19 18:50:34 +0200

    Ajout à l'interface de commande de askBidInformations(), pour créer une enchère.

--- src/main/java/com/projetenchere/Manager/View/commandLineInterface/ManagerCommandLineInterface.java
+++ src/main/java/com/projetenchere/Manager/View/commandLineInterface/ManagerCommandLineInterface.java
@@ -1,14 +1,50 @@
 package com.projetenchere.Manager.View.commandLineInterface;
 
 import com.projetenchere.Manager.View.IManagerUserInterface;
+import com.projetenchere.common.Model.Bid;
+import com.projetenchere.common.Model.Offer;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class ManagerCommandLineInterface implements IManagerUserInterface{
+    public static final Scanner scanner = new Scanner(System.in);
 
+    public ManagerCommandLineInterface(){
+
+    }
+
+    public void showMessage(String message)    {
+        System.out.println(message);
+    }
+
+    public String readMessage(){
+        return scanner.nextLine();
+    }
 
     @Override
-    public void displayPricesSorted(){
+    public Bid askBidInformations(){
+        String name = askBidName();
+        String description = askBidDescription();
+        return new Bid(name,description);
+    }
+
+    @Override
+    public String askBidDescription() {
+        showMessage("Veuillez saisir la description de l'enchère : ");
+        String description = readMessage();
+        return description;
+    }
+
+    @Override
+    public String askBidName(){
+        showMessage("Veuillez saisir le nom de l'enchère : ");
+        String name = readMessage();
+        return name;
+    }
+
+    @Override
+    public void displayPrices(List<Offer> DecryptedOffer){
 
     };
 
