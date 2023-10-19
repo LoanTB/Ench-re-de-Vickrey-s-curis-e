057b2b6706dbc0d9c37dfded8eb3db02ee9c2932
Loan Tremoulet Breton <loan.tremoulet-breton01@etu.umontpellier.fr>
2023-10-19 17:52:22 +0200

    Fermeture optimisée des connexions après utilisation

--- src/main/java/com/projetenchere/common/network/NetworkUtil.java
+++ src/main/java/com/projetenchere/common/network/NetworkUtil.java
@@ -11,17 +11,21 @@ import java.net.UnknownHostException;
 public class NetworkUtil {
 
     public static Object receive(int port) throws IOException, ClassNotFoundException {
-        ServerSocket serverSocket = new ServerSocket(port);
-        Socket clientSocket = serverSocket.accept();
-        ObjectInputStream objectInput = new ObjectInputStream(clientSocket.getInputStream());
-        return (Object) objectInput.readObject();
+        try (ServerSocket serverSocket = new ServerSocket(port);
+             Socket clientSocket = serverSocket.accept();
+             ObjectInputStream objectInput = new ObjectInputStream(clientSocket.getInputStream())) {
+
+            return objectInput.readObject();
+        }
     }
 
     public static void send(String serverAddress, int serverPort, Object data) throws IOException {
-        Socket clientSocket = new Socket(serverAddress, serverPort);
-        ObjectOutputStream objectOutput = new ObjectOutputStream(clientSocket.getOutputStream());
-        objectOutput.writeObject(data);
-        objectOutput.flush();
+        try (Socket clientSocket = new Socket(serverAddress, serverPort);
+             ObjectOutputStream objectOutput = new ObjectOutputStream(clientSocket.getOutputStream())) {
+
+            objectOutput.writeObject(data);
+            objectOutput.flush();
+        }
     }
 
     public static String getMyIP() throws UnknownHostException {
