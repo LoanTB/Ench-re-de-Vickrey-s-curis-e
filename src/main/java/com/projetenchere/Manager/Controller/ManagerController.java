3286852e3ff4f21caa0606a722819473429b64fa
auxilienk <auxilien@etud.fr>
2023-10-19 18:52:55 +0200

    Modification des updates pour afficher les prix déchiffrés (incomplet)

--- src/main/java/com/projetenchere/Manager/Controller/ManagerController.java
+++ src/main/java/com/projetenchere/Manager/Controller/ManagerController.java
@@ -3,14 +3,16 @@ package com.projetenchere.Manager.Controller;
 import com.projetenchere.Manager.ManagerApp;
 import com.projetenchere.Manager.View.IManagerUserInterface;
 import com.projetenchere.Manager.View.commandLineInterface.ManagerCommandLineInterface;
+import com.projetenchere.common.Model.BidStarter;
 import com.projetenchere.common.Model.Offer;
 import com.projetenchere.common.Model.Bid;
+import com.projetenchere.common.Model.Encrypted.EncryptedOffer;
 import com.projetenchere.common.Util.EncryptionUtil;
 
 import java.security.KeyPair;
 import java.security.PublicKey;
 import java.util.List;
-
+import java.util.ArrayList;
 
 public class ManagerController {
 
@@ -22,85 +24,91 @@ public class ManagerController {
         return ManagerKeys;
     }
 
-
     //Creer l'enchère.
 
     public Bid createBid()
     {
-        return new Bid();
+        return ui.askBidInformations();
     }
-
 
     //Lancer l'enchère donc l'envoyer aux Bidders + envoyer la clé publique.
 
     public void sendBidAndKey(Bid currentBid, PublicKey managerPublicKey)
     {
-        //TODO : SendObjects.
+        BidStarter currentBidStarter = new BidStarter(managerPublicKey,currentBid);
+
+        //TODO : SendObjects. ????
+    }
+
+
+    public void showPrices(List<Offer> DecryptedOffer){
+        ui.displayPrices(DecryptedOffer);
     }
 
 
     //Recevoir les offres.
-
-    /*
-    public List<EncryptedOffer> fetchEncryptedOffers()
+    public void fetchEncryptedOffers() //ADD : Retour : List<EncryptedOffer>
     {
         //TODO : FetchObjects ?
     }
-    */
 
     //Déchiffrer les offres.
-/*
-    public List<Offer> decryptEncryptedOffers(List<EncryptedOffer> ReceivedOffers){
+    public void decryptEncryptedOffers(List<EncryptedOffer> ReceivedOffers){ //ADD : retour List<Offer>
         //TODO : Decrypt each offers.
-        //return List<Offer> DecryptedOffers;
-
+        return ;
     }
 
-    public float getWinnerPrice(List<Offer> DecryptedOffers)
+    public float getWinnerPrice(List<Offer> DecryptedOffer)
     {
         //TODO : Adapt int[] to List<Offer>.
+        return 1;
     }
-*/
 
-    /*
-    private static int getWinnerPrice(int[] prices)
+/*
+    private static int[] getWinnerPrice(int[] prices_cypher_shuffled)
     {
+        int prices_decode[] = testDecode(prices_cypher_shuffled);
+        int nBidder = prices_decode.length;
+
+        System.out.println("Testgetwinner cypher shuffled" + Arrays.toString(prices_cypher_shuffled));
+        System.out.println("Testgetwinner prices decode " + Arrays.toString(prices_decode));
+
+
         System.out.println("Original : ");
-        for(int num : prices)
+        for(int num : prices_decode)
         {
             System.out.println(num + "");
         }
-        Arrays.sort(prices); //Tri du plus petit au plus grand.
+
+        int[] prices_decode_sorted = prices_decode.clone();
+
+        Arrays.sort(prices_decode_sorted); //Tri du plus petit au plus grand.
         System.out.println("Sorted : ");
 
-        for(int num : prices)
+        for(int num : prices_decode_sorted)
         {
             System.out.println(num + "");
         }
 
-        return prices[prices.length -2];
-    }
-
-    public static void priceProcessing(int[] BidderPrice)
-    {
-        boolean correct = false;
-        while(!correct) {
-            int winnerPrice = getWinnerPrice(BidderPrice);
-            System.out.println("Winner price is : " + winnerPrice);
-            correct = checkWinner();
+        int winnerPrice = prices_decode_sorted[nBidder -2];
+        int winnerCypher = 0;
+        for (int i = 0; i < nBidder ; i++)
+        {
+            if(prices_decode[i] == winnerPrice)
+            {
+                winnerCypher = prices_cypher_shuffled[i];
+                int[] winner_price_and_cypher = {winnerCypher, winnerPrice};
+                return winner_price_and_cypher;
+            }
         }
-    }
-    */
 
-    /*
-    public void showPrices(List<>){
-        ui.displayPrices(List<>);
-    }
+        int[] winner_price_and_cypher = {winnerCypher, winnerPrice};
+        return winner_price_and_cypher;
+    }*/
 
     public void sendWinnerAndPrice()
     {
-
+        //TODO : Envoyer le chiffré du gagnant (plus grand prix chiffré), et le prix gagnant.
     }
-     */
 
 }
--- src/main/java/com/projetenchere/Manager/View/IManagerUserInterface.java
+++ src/main/java/com/projetenchere/Manager/View/IManagerUserInterface.java
@@ -1,10 +1,18 @@
 package com.projetenchere.Manager.View;
 
 import com.projetenchere.Manager.View.commandLineInterface.ManagerCommandLineInterface;
+import com.projetenchere.common.Model.Bid;
+import com.projetenchere.common.Model.Offer;
+
+import java.util.List;
 
 public interface IManagerUserInterface {
 
-    public void displayPricesSorted();
+    public Bid askBidInformations();
+    public String askBidName();
+    public String askBidDescription();
+    public void displayPrices(List<Offer> DecryptedOffer);
     public void displayWinnerPrice();
+
 
 }
