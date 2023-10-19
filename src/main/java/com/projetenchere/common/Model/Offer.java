aa0578e52dae65e6f5358fa9c018f16193fda41a
Loan Tremoulet Breton <loan.tremoulet-breton01@etu.umontpellier.fr>
2023-10-19 16:09:03 +0200

    Modifications de Offer : Renommage de idBidder et mise en place de getters

--- src/main/java/com/projetenchere/common/Model/Offer.java
+++ src/main/java/com/projetenchere/common/Model/Offer.java
@@ -3,19 +3,27 @@ package com.projetenchere.common.Model;
 import java.text.DecimalFormat;
 
 public class Offer {
-    private final String ID_Bidder;
+    private final String idBidder;
     private final float value;
 
-    public Offer(String ID_Bidder, float value) {
-        this.ID_Bidder = ID_Bidder;
+    public Offer(String idBidder, float value) {
+        this.idBidder = idBidder;
         this.value = value;
     }
 
-    public Offer(String ID_Bidder, String value) throws NumberFormatException{
-        this.ID_Bidder = ID_Bidder;
+    public Offer(String idBidder, String value){// throws NumberFormatException
+        this.idBidder = idBidder;
         DecimalFormat df = new DecimalFormat();
         df.setMaximumFractionDigits(2);
         this.value = Float.parseFloat(df.format(value));
+    }
+
+    public String getIdBidder() {
+        return idBidder;
+    }
+
+    public float getValue() {
+        return value;
     }
 }
 
