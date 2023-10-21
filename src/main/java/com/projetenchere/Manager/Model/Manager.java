b54ac52fc94f27b5cf7a9b46ff90f87ca997c267
auxilienk <auxilien@etud.fr>
2023-10-21 23:56:31 +0200

    Traitement des warnings de Manager.java

--- src/main/java/com/projetenchere/Manager/Model/Manager.java
+++ src/main/java/com/projetenchere/Manager/Model/Manager.java
@@ -1,27 +1,17 @@
 package com.projetenchere.Manager.Model;
 
-import com.projetenchere.Manager.Controller.ManagerController;
-
-import java.io.*;
 import java.security.KeyPair;
 import java.security.PrivateKey;
 import java.security.PublicKey;
-import java.util.*;
 
 public class Manager {
-    private KeyPair ManagerKeys;
     private PrivateKey managerPrivateKey;
     private PublicKey managerPublicKey;
     private String sellerAddress;
 
-    public KeyPair getManagerKeys() {
-        return ManagerKeys;
-    }
-
     public void setManagerKeys(KeyPair managerKeys) {
-        ManagerKeys = managerKeys;
-        managerPrivateKey = ManagerKeys.getPrivate();
-        managerPublicKey = ManagerKeys.getPublic();
+        managerPrivateKey = managerKeys.getPrivate();
+        managerPublicKey = managerKeys.getPublic();
     }
 
     public PrivateKey getManagerPrivateKey() {
