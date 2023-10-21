e7ac9a9b99ffd7578eeb19322b95add75930d067
Vachalde <remi.vachalde@etu.umontpellier.fr>
2023-10-21 23:36:12 +0200

    mini modif

--- src/main/java/com/projetenchere/common/network/ObjectSender.java
+++ src/main/java/com/projetenchere/common/network/ObjectSender.java
@@ -3,13 +3,13 @@ package com.projetenchere.common.network;
 import java.io.Serializable;
 
 public class ObjectSender implements Serializable {
-    private final String IP_serder;
+    private final String IP_sender;
     private final int PORT_sender;
     private final Object object;
     private final Class<?> objectClass;
 
-    public ObjectSender(String IP_serder, int PORT_sender, Object object, Class<?> objectClass) {
-        this.IP_serder = IP_serder;
+    public ObjectSender(String IP_sender, int PORT_sender, Object object, Class<?> objectClass) {
+        this.IP_sender = IP_sender;
         this.PORT_sender = PORT_sender;
         if (!objectClass.isInstance(object)){
             throw new IllegalArgumentException("Le type spécifié n'est pas le type de l'objet");
@@ -18,8 +18,8 @@ public class ObjectSender implements Serializable {
         this.objectClass = objectClass;
     }
 
-    public String getIP_serder() {
-        return IP_serder;
+    public String getIP_sender() {
+        return IP_sender;
     }
 
     public int getPORT_sender() {
