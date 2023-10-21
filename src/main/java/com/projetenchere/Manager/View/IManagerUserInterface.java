d1d9856c4b52d2969e2763d54ebf658382263045
auxilienk <auxilien@etud.fr>
2023-10-22 00:00:57 +0200

    Traitement des warnings de IManagerUserInterface.java

--- src/main/java/com/projetenchere/Manager/View/IManagerUserInterface.java
+++ src/main/java/com/projetenchere/Manager/View/IManagerUserInterface.java
@@ -3,18 +3,17 @@ package com.projetenchere.Manager.View;
 import com.projetenchere.common.Model.Bid;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Set;
 
 public interface IManagerUserInterface {
 
-    public Bid askBidInformations();
-    public String askBidName();
-    public String askBidDescription();
-    public LocalDateTime askBidEndTime();
-    public String askSellerAdress();
-    public void displayPrices(Set<Double> DecryptedPrice);
-    public void displayWinnerPrice();
+     Bid askBidInformations();
+     String askBidName();
+     String askBidDescription();
+     LocalDateTime askBidEndTime();
+     String askSellerAdress();
+     void displayPrices(Set<Double> DecryptedPrice);
+     void displayWinnerPrice();
 
 
 }
