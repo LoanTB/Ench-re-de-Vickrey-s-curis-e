85ab327e3dd41dcc5fd258d345f600c7165e71cf
Loan Tremoulet Breton <loan.tremoulet-breton01@etu.umontpellier.fr>
2023-10-22 17:01:53 +0200

    Ajout d'un test pour receiveOffersUntilBidEnd

--- src/test/java/com/projetenchere/Seller/Controller/SellerControllerTest.java
+++ src/test/java/com/projetenchere/Seller/Controller/SellerControllerTest.java
@@ -1,11 +1,16 @@
 package com.projetenchere.Seller.Controller;
 
 import com.projetenchere.common.Model.Bid;
+import com.projetenchere.common.Model.Encrypted.EncryptedOffer;
+import com.projetenchere.common.Model.Offer;
 import com.projetenchere.common.Model.Sendable.ObjectSender;
+import com.projetenchere.common.Util.EncryptionUtil;
 import com.projetenchere.common.Util.NetworkUtil;
 import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.net.UnknownHostException;
+import java.security.KeyPair;
+import java.security.PublicKey;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -16,18 +21,21 @@ public class SellerControllerTest {
     Thread serverThread;
     Thread clientThread;
 
+    final String localhost = "127.0.0.1";
+
+    final int BidderPort = 24681;
+    final int SellerPort = 24682;
+    final int managerPort = 24683;
+
     @Test
     public void test_fetchCurrentBid() throws InterruptedException, UnknownHostException {
         SellerController controller = new SellerController();
-        String SellerIp = NetworkUtil.getMyIP();
-        int SellerPort = 24682;
-
 
         serverThread = new Thread(() -> {
             Bid data = new Bid("TEST","TEST", LocalDateTime.parse("22-10-2023 16:00:00", DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss")));
-            ObjectSender objectSender = new ObjectSender(SellerIp,SellerPort,data,data.getClass());
+            ObjectSender objectSender = new ObjectSender(localhost,SellerPort,data,data.getClass());
             try {
-                NetworkUtil.send(SellerIp,SellerPort,objectSender);
+                NetworkUtil.send(localhost,SellerPort,objectSender);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
@@ -35,6 +43,54 @@ public class SellerControllerTest {
 
         clientThread = new Thread(() -> {
             controller.fetchCurrentBid();
+        });
+
+        serverThread.start();
+        clientThread.start();
+
+        serverThread.join();
+        clientThread.join();
+
+        assertEquals(controller.getCurrentBid().getBidDescription(),"TEST");
+    }
+
+    @Test
+    public void test_receiveOffersUntilBidEnd() throws InterruptedException, UnknownHostException {
+        SellerController controller = new SellerController();
+
+        serverThread = new Thread(() -> {
+            Bid bid = new Bid("TEST", "TEST", LocalDateTime.now().plusSeconds(5));
+            ObjectSender objectSender = new ObjectSender(localhost, SellerPort, bid, bid.getClass());
+            try {
+                NetworkUtil.send(localhost, SellerPort, objectSender);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            KeyPair managerKeys = null;
+            try {
+                managerKeys = EncryptionUtil.generateKeyPair();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            PublicKey manager_publicKey = managerKeys.getPublic();
+            Offer offer = new Offer("Pablo", 10.0);
+            EncryptedOffer encryptedOffer;
+            try {
+                encryptedOffer = new EncryptedOffer(offer, manager_publicKey);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            objectSender = new ObjectSender(localhost, SellerPort, encryptedOffer, encryptedOffer.getClass());
+            try {
+                NetworkUtil.send(localhost, SellerPort, objectSender);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        clientThread = new Thread(() -> {
+            controller.fetchCurrentBid();
+            controller.receiveOffersUntilBidEnd();
         });
 
         serverThread.start();
