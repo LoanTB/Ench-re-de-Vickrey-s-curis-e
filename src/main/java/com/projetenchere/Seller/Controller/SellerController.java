64dcfd46d0e034c0416594c3d841af9a83efd365
Loan Tremoulet Breton <loan.tremoulet-breton01@etu.umontpellier.fr>
2023-10-22 13:15:18 +0200

    Simplification de APP en transférant toute la logique dans controllers

--- src/main/java/com/projetenchere/Seller/Controller/SellerController.java
+++ src/main/java/com/projetenchere/Seller/Controller/SellerController.java
@@ -1,5 +1,6 @@
 package com.projetenchere.Seller.Controller;
 
+import com.projetenchere.Seller.Model.Seller;
 import com.projetenchere.Seller.View.ISellerUserInterface;
 import com.projetenchere.Seller.View.commandLineInterface.SellerCommandLineInterface;
 import com.projetenchere.common.Model.Bid;
@@ -9,6 +10,7 @@ import com.projetenchere.common.Model.Serializable.ObjectSender;
 import com.projetenchere.common.Model.Winner;
 
 import java.io.IOException;
+import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
 import java.util.*;
 
@@ -16,7 +18,36 @@ public class SellerController {
 
     private static final ISellerUserInterface ui = new SellerCommandLineInterface();
     private Bid currentBid;
+    private final Seller seller = new Seller();
+
+    private Winner winner = null;
     SellerNetworkController networkController = new SellerNetworkController();
+
+    public Bid getCurrentBid() {
+        return currentBid;
+    }
+
+    public void requestCurrentBid(){
+        this.currentBid = networkController.getBidRequest();
+    }
+
+    public boolean auctionInProgress(){
+        return (!this.currentBid.isOver());
+    }
+
+    public void receiveOffersUntilAuctionEnd(){
+        EncryptedOffer offerReceived;
+        while (auctionInProgress()){
+            try{
+                ObjectSender request = getEncryptedOfferRequests();
+                offerReceived = (EncryptedOffer) request.getObjectClass().cast(request.getObject());
+                seller.addEncryptedOffer(offerReceived);
+                seller.addBidderIp(request.getIP_sender());
+                seller.addBidderPort(request.getPORT_sender());
+                displayOfferReceived(offerReceived);
+            } catch (IOException | ClassNotFoundException e){}
+        }
+    }
 
     public SellerController() throws UnknownHostException {}
 
@@ -28,7 +59,9 @@ public class SellerController {
         ui.displayOfferReceived(encryptedOffer);
     }
 
-    public void displayWinner(List<EncryptedOffer> encryptedOffers, List<Double> biddersWinStatus, Winner winner){
+    public void displayWinner(){
+        List<EncryptedOffer>  encryptedOffers = seller.getEncryptedOffers();
+        List<Double> biddersWinStatus = getBiddersWinStatus();
         String winnerID = this.getWinnerID(encryptedOffers,biddersWinStatus);
         Double price = winner.getPriceToPay();
         ui.displayWinner(winnerID,price);
@@ -54,19 +87,18 @@ public class SellerController {
         return networkController.getEncryptedOfferRequests();
     }
 
-    public void sendEncryptedPrices(List<EncryptedOffer> offers) throws IOException {
-        networkController.sendEncryptedPrices(getEncryptedPrices(offers));
+    public void sendEncryptedPrices() throws IOException {
+        networkController.sendEncryptedPrices(getEncryptedPrices(seller.getEncryptedOffers()));
     }
 
-    public Bid getBid() {
-        return networkController.getBidRequest();
+    public void requestWinner() {
+        winner = networkController.getWinnerRequest();
     }
 
-    public Winner getWinner() {
-        return networkController.getWinnerRequest();
-    }
-
-    public void sendResults(List<String> IPs, List<Integer> PORTs, List<Double> results) throws IOException {
+    public void sendResults() throws IOException {
+        List<String> IPs = seller.getBiddersIps();
+        List<Integer> PORTs = seller.getBiddersPorts();
+        List<Double> results = getBiddersWinStatus();
         if (IPs.size() == PORTs.size() && IPs.size() == results.size()){
             for (int i=0;i< IPs.size();i++){
                 networkController.send(IPs.get(i),PORTs.get(i),results.get(i));
@@ -76,7 +108,8 @@ public class SellerController {
         }
     }
 
-    public List<Double> getBiddersWinStatus(List<EncryptedOffer> encryptedOffers, Winner winner){
+    public List<Double> getBiddersWinStatus(){
+        List<EncryptedOffer> encryptedOffers = seller.getEncryptedOffers();
         boolean haveAWinner = false;
         List<Double> winStatus = new ArrayList<>();
         for (int i=0;i<encryptedOffers.size();i++){
--- src/main/java/com/projetenchere/Seller/SellerApp.java
+++ src/main/java/com/projetenchere/Seller/SellerApp.java
@@ -1,46 +1,29 @@
 package com.projetenchere.Seller;
 
 import com.projetenchere.Seller.Controller.SellerController;
-import com.projetenchere.Seller.Model.Seller;
-import com.projetenchere.common.Model.Bid;
-import com.projetenchere.common.Model.Encrypted.EncryptedOffer;
-import com.projetenchere.common.Model.Serializable.ObjectSender;
-import com.projetenchere.common.Model.Winner;
 
 import java.io.IOException;
-import java.net.SocketTimeoutException;
 import java.util.List;
 
 public class SellerApp {
     public static void main(String[] args) throws ClassNotFoundException, IOException {
-        Seller seller = new Seller();
         SellerController controller = new SellerController();
 
         controller.diplayHello();
 
-        Bid actualBid = controller.getBid();
+        controller.requestCurrentBid();
 
-        EncryptedOffer offerReceived;
-        while (!actualBid.isOver()){
-            try{
-                ObjectSender request = controller.getEncryptedOfferRequests();
-                offerReceived = (EncryptedOffer) request.getObjectClass().cast(request.getObject());
-                seller.addEncryptedOffer(offerReceived);
-                seller.addBidderIp(request.getIP_sender());
-                seller.addBidderPort(request.getPORT_sender());
-                controller.displayOfferReceived(offerReceived);
-            } catch (SocketTimeoutException e){}
-        }
+        controller.receiveOffersUntilAuctionEnd();
 
-        controller.sendEncryptedPrices(seller.getEncryptedOffers());
+        controller.sendEncryptedPrices();
+
         controller.displayEncryptedPriceSended();
 
-        Winner winner = controller.getWinner();
+        controller.requestWinner();
 
-        List<Double> biddersWinStatus = controller.getBiddersWinStatus(seller.getEncryptedOffers(),winner);
-        controller.displayWinner(seller.getEncryptedOffers(),biddersWinStatus,winner);
+        controller.displayWinner();
 
-        controller.sendResults(seller.getBiddersIps(),seller.getBiddersPorts(),biddersWinStatus);
+        controller.sendResults();
 
         controller.displayResultsSent();
     }
