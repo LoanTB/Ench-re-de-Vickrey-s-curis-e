b2e688ba083760c51041032b10356153b3873789
auxilienk <auxilien@etud.fr>
2023-10-22 17:12:55 +0200

    Tests effectués pour getwinnerprice()

--- src/test/java/com/projetenchere/Manager/ManagerAppTest.java
+++ src/test/java/com/projetenchere/Manager/ManagerAppTest.java
@@ -1,9 +1,11 @@
 package com.projetenchere.Manager;
 
 import com.projetenchere.Manager.Controller.ManagerController;
+import com.projetenchere.common.Util.EncryptionUtil;
 import org.junit.jupiter.api.Test;
 
 import java.security.PublicKey;
+import java.util.Base64;
 import java.util.HashSet;
 import com.projetenchere.common.Model.Winner;
 import java.util.Set;
@@ -33,13 +35,32 @@ public class ManagerAppTest {
         PublicKey key = controller.manager.getManagerPublicKey();
         Set<Double> prices = new HashSet<>();
         prices.add(5.1);
-        //prices.add(6.8);
-        //prices.add(39.3);
-        //prices.add(25.2);
 
         Winner win = controller.getWinnerPrice(key,prices);
-        controller.showWinnerPrice(win);
 
+        Double check = EncryptionUtil.decrypt(win.getEncryptedMaxprice(),controller.manager.getManagerPrivateKey());
+
+        assert check == win.getPriceToPay() : "Les prix doivent être pareil";
+        System.out.println("Prix chiffré de winner et le prix à payer de winner sont différents.");
+    }
+
+    @Test
+    public void test_getwinnerprice_withTwoBidder() throws Exception{
+        ManagerController controller = new ManagerController();
+        controller.generateManagerKeys();
+        PublicKey key = controller.manager.getManagerPublicKey();
+        Set<Double> prices = new HashSet<>();
+        prices.add(5.1);
+        prices.add(6.8);
+
+        Winner win = controller.getWinnerPrice(key,prices);
+        //controller.showWinnerPrice(win);
+        //System.out.println(Base64.getEncoder().encodeToString(win.getEncryptedMaxprice()));
+        Double check = EncryptionUtil.decrypt(win.getEncryptedMaxprice(),controller.manager.getManagerPrivateKey());
+        //System.out.println(check);
+
+        assert check != win.getPriceToPay() : "Les prix ne doivent pas être pareil";
+        System.out.println("Prix chiffré de winner et le prix à payer de winner sont différents.");
     }
 
     @Test
