d06c9656b29e9af098f25a346fc83398b9191e0c
Loan Tremoulet Breton <loan.tremoulet-breton01@etu.umontpellier.fr>
2023-11-14 22:39:11 +0100

    fix: Ajustement main Manager

--- src/main/java/com/projetenchere/Manager/ManagerApp.java
+++ src/main/java/com/projetenchere/Manager/ManagerApp.java
@@ -9,18 +9,14 @@ public class ManagerApp {
     public static void main(String[] args) throws Exception {
         System.out.println("Bienvenue Manager !");
         ManagerController controller = new ManagerController();
-        Bid currentBid = controller.initBid();
+        controller.initBid();
         System.out.println("Génération de clé...");
         controller.generateManagerKeys();
         System.out.println("Lancement de l'enchère ...");
         controller.launchBids();
-        System.out.println("Reception des prix ...");
-        EncryptedPrices pricesReceived = controller.waitEncryptedPrices();
-        System.out.println("Traitement des prix...");
-        Winner winnerForCurrentBid = controller.processPrices(pricesReceived);
-        controller.showWinnerPrice(winnerForCurrentBid);
-        System.out.println("Envoie du prix gagnant...");
-        controller.endBid(winnerForCurrentBid);
-        System.out.println("Fin des enchères !");
+//        System.out.println("Reception des prix ..."); TODO : A incorporé dans les view
+//        System.out.println("Traitement des prix...");
+//        System.out.println("Envoie du prix gagnant...");
+//        System.out.println("Fin des enchères !");
     }
 }
