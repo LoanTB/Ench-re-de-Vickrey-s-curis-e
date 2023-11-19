638292e0095e3004bf7901b7009cf03bc87949b0
auxilienk <auxilien@etud.fr>
2023-11-19 15:42:03 +0100

    <feature> [Sprint2] : Ajouts de to do.

--- src/main/java/com/projetenchere/Manager/ManagerApp.java
+++ src/main/java/com/projetenchere/Manager/ManagerApp.java
@@ -6,26 +6,22 @@ public class ManagerApp {
     public static void main(String[] args) throws Exception {
         ManagerController controller = new ManagerController();
         controller.diplayHello();
+        //TODO : Generer mes clés.
+        controller.generateManagerKeys();
+
         controller.initConnexion();
 
-
         //Recevoir l'enchère créé de seller.
-        //TODO : Recevoir l'encère créé de seller
-        //TODO : "Supprimer" les méthoides de création par manager.
+        //TODO : Recevoir l'enchère créé de seller
+        //TODO : "Supprimer" les méthodes de création par manager.
 
             // Enregistrer les informations de seller.
             //Envoyer mes informations au seller.
         controller.initContactWithSeller();
 
+//        controller.initBid(); //TODO : Seller qui fait ça mtn ?
 
-        controller.initBid();
-        System.out.println("Génération de clé...");
-        controller.generateManagerKeys();
-        System.out.println("Lancement de l'enchère ...");
         controller.launchBids();
-//        System.out.println("Reception des prix ..."); TODO : A incorporé dans les view
-//        System.out.println("Traitement des prix...");
-//        System.out.println("Envoie du prix gagnant...");
-//        System.out.println("Fin des enchères !");
+
     }
 }
