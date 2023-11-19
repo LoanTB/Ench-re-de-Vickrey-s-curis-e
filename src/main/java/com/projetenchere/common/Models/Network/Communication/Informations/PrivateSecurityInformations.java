d322b87dcb5bd89193dc17779b971f6a98813c9c
Loan Tremoulet Breton <loan.tremoulet-breton01@etu.umontpellier.fr>
2023-11-19 15:08:58 +0100

    feat: Add auto id to privateSI

--- src/main/java/com/projetenchere/common/Models/Network/Communication/Informations/PrivateSecurityInformations.java
+++ src/main/java/com/projetenchere/common/Models/Network/Communication/Informations/PrivateSecurityInformations.java
@@ -1,17 +1,22 @@
 package com.projetenchere.common.Models.Network.Communication.Informations;
 
 import java.security.KeyPair;
+import java.util.UUID;
 
 public class PrivateSecurityInformations {
+    private final String id;
     private final NetworkContactInformation networkContactInformation;
     private final KeyPair signatureKeys;
     private final KeyPair encryptionKeys;
 
     public PrivateSecurityInformations(NetworkContactInformation networkContactInformation, KeyPair signatureKeys, KeyPair encryptionKeys) {
+        this.id = UUID.randomUUID().toString();
         this.networkContactInformation = networkContactInformation;
         this.signatureKeys = signatureKeys;
         this.encryptionKeys = encryptionKeys;
     }
+
+    public String getId(){return id;}
 
     public NetworkContactInformation getNetworkContactInformation() {
         return networkContactInformation;
