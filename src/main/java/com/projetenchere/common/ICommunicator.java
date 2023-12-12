a6be325d940e55cd497b80800b5e36172e38371f
Paul Jacquemin <paul.jacquemin@etu.umontpellier.fr>
2023-12-12 11:52:17 +0100

    (WIP) refactor: enlevé le type de retour de handleDataFromParty car data est handlé dans la méthode

--- src/main/java/com/projetenchere/common/ICommunicator.java
+++ src/main/java/com/projetenchere/common/ICommunicator.java
@@ -7,6 +7,6 @@ import java.io.Serializable;
 
 public interface ICommunicator {
 
-    void sendDataToParty(Party party, DataWrapper<? extends Serializable> data);
-    DataWrapper<? extends Serializable> waitForDataFromParty(Party party, IDataHandler handler);
+    void sendDataToParty(DataWrapper<? extends Serializable> data);
+    void handleDataFromParty(IDataHandler handler);
 }
