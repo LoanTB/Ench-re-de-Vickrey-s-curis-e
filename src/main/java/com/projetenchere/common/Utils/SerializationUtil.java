023743bae48507b8737a00a66d02ef46974880c4
Paul Jacquemin <paul.jacquemin@umontpellier.fr>
2023-12-16 17:12:04 +0100

    (WIP) refactor: fait en sorte que SerializationUtil lise et écrive les objets sur les stream des socket

--- src/main/java/com/projetenchere/common/Utils/SerializationUtil.java
+++ src/main/java/com/projetenchere/common/Utils/SerializationUtil.java
@@ -1,19 +1,34 @@
 package com.projetenchere.common.Utils;
 
+import com.projetenchere.common.network.DataWrapper;
+import com.projetenchere.common.network.NetworkData;
+
+import javax.net.ssl.SSLSocket;
 import java.io.*;
 
 public class SerializationUtil {
-    public static byte[] serialize(Object obj) throws IOException {
-        try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-             ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream)) {
-            objectOutputStream.writeObject(obj);
-            return byteArrayOutputStream.toByteArray();
-        }
+    public static void serializeTo(NetworkData data, SSLSocket socket) throws IOException {
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+            objectOutputStream.writeObject(data);
     }
-    public static Object deserialize(byte[] bytes) throws IOException, ClassNotFoundException {
-        try (ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
-             ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream)) {
-            return objectInputStream.readObject();
+    public static NetworkData deserializeAsNetworkDataFrom(SSLSocket socket) throws IOException, ClassNotFoundException {
+        ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+        Object rawData = objectInputStream.readObject();
+        NetworkData data;
+        try {
+            data = (NetworkData) rawData;
+        } catch (ClassCastException e) {
+            data = NetworkData.error();
+        }
+        return data;
+    }
+
+    public static <T extends Serializable> DataWrapper<T> deserializeAsDataWrapper(SSLSocket socket) throws IOException, ClassNotFoundException {
+        NetworkData data = deserializeAsNetworkDataFrom(socket);
+        try {
+            return (DataWrapper<T>) data;
+        } catch (ClassCastException e) {
+            throw new RuntimeException("Could not cast to data");
         }
     }
 
