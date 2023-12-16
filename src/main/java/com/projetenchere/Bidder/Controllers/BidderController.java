d0667ebf7cd562a6ffdc9db27a33d82662ddbf8b
Paul Jacquemin <paul.jacquemin@umontpellier.fr>
2023-12-16 17:12:53 +0100

    (WIP) refactor: adapté le controller de Bidder

--- src/main/java/com/projetenchere/Bidder/Controllers/BidderController.java
+++ src/main/java/com/projetenchere/Bidder/Controllers/BidderController.java
@@ -3,42 +3,59 @@ package com.projetenchere.Bidder.Controllers;
 import com.projetenchere.Bidder.Model.Bidder;
 import com.projetenchere.Bidder.View.IBidderUserInterface;
 import com.projetenchere.Bidder.View.commandLineInterface.BidderCommandLineInterface;
-import com.projetenchere.Bidder.network.BidderCommunications;
-import com.projetenchere.Manager.Model.Manager;
+import com.projetenchere.Bidder.network.BidderClient;
 import com.projetenchere.common.Controllers.Controller;
-import com.projetenchere.common.Models.Identity;
 import com.projetenchere.common.Models.Network.Communication.CurrentBids;
 import com.projetenchere.common.Models.Encrypted.EncryptedOffer;
-import com.projetenchere.common.Models.Network.Communication.CurrentBidsPublicKeys;
 import com.projetenchere.common.Models.Network.Communication.WinStatus;
 import com.projetenchere.common.Models.Offer;
-import com.projetenchere.common.network.Party;
-import com.projetenchere.common.Utils.NetworkUtil;
 
+import java.security.PublicKey;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 import static java.lang.Thread.sleep;
 
 public class BidderController extends Controller {
     private final IBidderUserInterface ui = new BidderCommandLineInterface();
-    private CurrentBids currentBids = null;
-    private CurrentBidsPublicKeys currentBidsPublicKeys = null;
+    private CurrentBids currentBids;
     private final List<String> participatedBid = new ArrayList<>();
     private final List<WinStatus> results = new ArrayList<>();
     private final Bidder bidder = new Bidder();
-    private final Manager manager = new Manager(NetworkUtil.getManagerSocketAddress());
-    BidderCommunications communications = new BidderCommunications();
+    BidderClient client = new BidderClient();
+    private PublicKey managerPubKey;
 
     public void setCurrentBids(CurrentBids currentBids) {
         this.currentBids = currentBids;
     }
 
-    public void setCurrentBidsPublicKeys(CurrentBidsPublicKeys currentBidsPublicKeys) {
-        this.currentBidsPublicKeys = currentBidsPublicKeys;
+    public void askForManagerPubKey(){
+        ui.tellWaitManagerSecurityInformations();
+        managerPubKey = client.getManagerPubKey();
     }
 
+    public void askForCurrentBids() {
+        currentBids = client.getCurrentBids();
+    }
+
+    public void initWithManager() {
+        client.connectToManager();
+        askForManagerPubKey();
+        askForCurrentBids();
+    }
+
+    public void showBids() {
+        ui.displayBid(this.currentBids);
+    }
+
+    public void readAndSendOffer() throws Exception {
+        Offer offer = ui.readOffer(bidder, currentBids);
+        EncryptedOffer encryptedOffer = new EncryptedOffer(offer, managerPubKey);
+        participatedBid.add(offer.getIdBid());
+        //client.connectToSeller();
+        ui.tellOfferSent();
+
+    }
 
     public void addResult(WinStatus result){
         results.add(result);
@@ -48,70 +65,9 @@ public class BidderController extends Controller {
         return participatedBid;
     }
 
-    public void readInfos() throws Exception {
-        bidder.setIdentity(new Identity(UUID.randomUUID().toString(),ui.readName(),ui.readSurname(),"Bidder"));
-    }
-
-    public void waitToReceiveBids() {
-        ui.tellWaitBidsAnnoncement();
-        while (currentBids == null) {
-            waitSychro(1000);
-        }
-    }
-
-    public void waitToReceiveBidsPublicKeys() {
-        ui.tellWaitBidsPublicKeysAnnoncement();
-        while (currentBidsPublicKeys == null) {
-            waitSychro(1000);
-        }
-    }
-
-    public void showBid() {
-        waitToReceiveBids();
-        ui.displayBid(this.currentBids);
-    }
-
-    public void readAndSendOffer() throws Exception {
-        Offer offer = ui.readOffer(bidder, currentBids);
-        waitToReceiveBidsPublicKeys();
-        EncryptedOffer encryptedOffer = new EncryptedOffer(offer, currentBidsPublicKeys.getKeyOfBid(offer.getIdBid()));
-        networkController.sendTo(currentBids.getBid(offer.getIdBid()).getSeller().getIdentity().getId(),encryptedOffer);
-        participatedBid.add(offer.getIdBid());
-        ui.tellOfferSent();
-    }
-
-    public void waitForPrice() {
-        ui.tellWaitOfferResult();
-        while (results.isEmpty()) {
-            waitSychro(1000);
-        }
-        if (results.get(0).isWinner()){
-            ui.tellOfferWon(results.get(0).getPrice());
-        } else {
-            ui.tellOfferLost();
-        }
-    }
-
-    public void askForManagerPubKey(){
-        ui.tellWaitManagerSecurityInformations();
-        while (!networkController.informationContainsPublicKeys("Manager")) {
-            waitSychro(1000);
-        }
-    }
-
-    public void sendBidderInfosToManager() throws Exception {
-        Party manager =
-    }
-
     public void displayHello(){ui.displayHello();}
 
     public IBidderUserInterface getUi() {
         return ui;
-    }
-
-    public void sendRequestOffers() throws Exception {
-        askForManagerPubKey();
-        ui.tellSendRequestOffers();
-        networkController.sendTo("Manager","InitPackageRequest");
     }
 }
