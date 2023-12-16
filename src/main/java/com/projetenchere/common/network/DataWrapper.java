e03e5b18a5e89e207c99f2ffa1e7fd17fac70d4d
Paul Jacquemin <paul.jacquemin@umontpellier.fr>
2023-12-16 17:09:44 +0100

    (WIP) feat: ajouté des signatures au data wrapper

--- src/main/java/com/projetenchere/common/network/DataWrapper.java
+++ src/main/java/com/projetenchere/common/network/DataWrapper.java
@@ -1,12 +1,15 @@
 package com.projetenchere.common.network;
 
+import org.jetbrains.annotations.Nullable;
+
 import java.io.Serializable;
+import java.security.Signature;
 
 public class DataWrapper<T extends Serializable> extends NetworkData {
     private final T object;
 
-    public DataWrapper(T object, NetworkDataHeaders header) {
-        super(header);
+    public DataWrapper(T object, Headers header, @Nullable Signature signature) {
+        super(header, signature);
         this.object = object;
     }
 
