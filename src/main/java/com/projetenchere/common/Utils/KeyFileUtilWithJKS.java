f5aee359583eb6565f2bcf4a08e009034ce8a695
auxilienk <auxilien@etud.fr>
2023-12-17 13:35:48 +0100

    <feat> [Sprint3] : Renommage de methode principale generateAndSave ... ajout de getFilePath, Erreur : Première ocmmande pour créer le keystore n'a aucun effets

--- src/main/java/com/projetenchere/common/Utils/KeyFileUtilWithJKS.java
+++ src/main/java/com/projetenchere/common/Utils/KeyFileUtilWithJKS.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.security.KeyStore;
 import java.io.FileInputStream;
+import java.util.Objects;
 
 public class KeyFileUtilWithJKS implements I_KeyFileUtil {
     private static final String KEYSTORE_ALIAS = "SecureWin";
@@ -19,17 +20,10 @@ public class KeyFileUtilWithJKS implements I_KeyFileUtil {
     private static final String KEY_ALGORITHM = "RSA";
     private static final int KEY_SIZE = 2048;
     private static final int VALIDITY_DAYS = 365;
-    private static final String KEYSTORE_FILEPATH = "src/main/resources/config/config_signature_keypair.jks";
-    private static final  String CERT_FILEPATH = "src/main/resources/config/config_signature_certificat.cer"; 
+    private static final String KEYSTORE_FILENAME = "config_signature_keypair.jks";
+    private static final  String CERT_FILENAME = "config_signature_certificat.cer";
     private static final String KEYSTORE_PASSWORD = "SecureWinPaulLoanYukiRemiKatia";
-    private static final String PRIVKEY_ALIAS = "privateKeySecureWin"; 
-    private static final String PUBKEY_ALIAS = "publicKeySecureWin"; 
 
-
-    public KeyFileUtilWithJKS(){
-
-    }
-    
     private static void executeCommand(String command) throws IOException {
         Process process = Runtime.getRuntime().exec(command);
         BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
@@ -40,58 +34,58 @@ public class KeyFileUtilWithJKS implements I_KeyFileUtil {
         }
         reader.close();
     }
-    
-    public void generateCertificateByCommand(){
-        try {
-            // Commande pour générer une paire de clés dans .jks
-            String genKeyCommand = "keytool -genkeypair -alias "+ KEYSTORE_ALIAS +" -keyalg "+KEY_ALGORITHM+" -keysize "+KEY_SIZE+" -keystore "+KEYSTORE_FILEPATH+" -validity "+VALIDITY_DAYS+" -dname \"CN=Secure, OU=Win, O=SecureWin, L=Montpellier, ST=Occitanie, C=FR\" -storepass "+KEYSTORE_PASSWORD+" -keypass "+KEYSTORE_PASSWORD;
-            executeCommand(genKeyCommand);
 
-            // Commande pour exporter le certificat associé au .jks
-            String exportCertCommand = "keytool -export -alias "+ KEYSTORE_ALIAS +" -file "+CERT_FILEPATH+" -keystore "+KEYSTORE_FILEPATH+" -storepass "+KEYSTORE_PASSWORD+" -keypass "+KEYSTORE_PASSWORD;
-            executeCommand(exportCertCommand);
-        } catch (IOException e) {
+    private String getFilePath(String fileName){
+        File ressources = new File("");
+
+        File configFile = new File(ressources.getAbsolutePath()+"/src/main/resources/config/"+fileName);
+
+        String filepath = configFile.getAbsolutePath();
+        System.out.println(filepath);
+        return filepath;
+    }
+
+    @Override
+    public void generateAndSaveKeyPair(){
+        try {
+            try {
+
+                String KEYSTORE_FILEPATH = getFilePath(KEYSTORE_FILENAME);
+
+                String CERT_FILEPATH =  getFilePath(CERT_FILENAME);
+
+                // Commande pour générer une paire de clés dans .jks
+                String genKeyCommand = "keytool -genkeypair -alias "+ KEYSTORE_ALIAS +" -keyalg "+KEY_ALGORITHM+" -keysize "+KEY_SIZE+" -keystore "+KEYSTORE_FILEPATH+" -validity "+VALIDITY_DAYS+" -dname \"CN=Secure, OU=Win, O=SecureWin, L=Montpellier, ST=Occitanie, C=FR\" -storepass "+KEYSTORE_PASSWORD+" -keypass "+KEYSTORE_PASSWORD;
+                System.out.println(genKeyCommand);
+                executeCommand(genKeyCommand);
+                System.out.println("Commande de generation de keystore reussie !");
+
+                // Commande pour exporter le certificat associé au .jks
+                String exportCertCommand = "keytool -export -alias "+ KEYSTORE_ALIAS +" -file "+CERT_FILEPATH+" -keystore "+KEYSTORE_FILEPATH+" -storepass "+KEYSTORE_PASSWORD+" -keypass "+KEYSTORE_PASSWORD;
+                System.out.println(exportCertCommand);
+
+                executeCommand(exportCertCommand);
+                System.out.println("Commande de generation de certificat reussie !");
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     @Override
-    public void saveKeyPair(){
-            try {
-                
-                generateCertificateByCommand();
-
-                // Charger le certificat depuis un fichier
-                FileInputStream fis = new FileInputStream(CERT_FILEPATH);
-                CertificateFactory cf = CertificateFactory.getInstance(CERTIFICATE_TYPE);
-                Certificate certificateChain[] = new Certificate[1];
-                certificateChain[0] = cf.generateCertificate(fis);
-                fis.close();
-
-                // Charger le keystore
-                KeyStore keystore = KeyStore.getInstance(KEYSTORE_TYPE);
-                keystore.load(new FileInputStream(KEYSTORE_FILEPATH), KEYSTORE_PASSWORD.toCharArray());
-
-                // Ajouter le certificat au KeyStore avec un alias spécifique
-                keystore.setKeyEntry(PRIVKEY_ALIAS,getPrivateKey(KEYSTORE_FILEPATH, KEYSTORE_PASSWORD),KEYSTORE_PASSWORD.toCharArray(),certificateChain);
-
-                // Sauvegarder le KeyStore mis à jour dans un fichier (si nécessaire)
-                keystore.store(new FileOutputStream(KEYSTORE_FILEPATH), KEYSTORE_PASSWORD.toCharArray());
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-
-
-
-    @Override
     public boolean isKeyPairSaved(){
         try {
+
+            String KEYSTORE_FILEPATH = getFilePath(KEYSTORE_FILENAME);
+            String CERT_FILEPATH =  getFilePath(CERT_FILENAME);
+
             KeyStore keyStore = KeyStore.getInstance(KEYSTORE_TYPE);
             keyStore.load(new FileInputStream(KEYSTORE_FILEPATH), KEYSTORE_PASSWORD.toCharArray());
-            return keyStore.containsAlias(PRIVKEY_ALIAS);
+            return keyStore.containsAlias(KEYSTORE_ALIAS);
         } catch (Exception e) {
             e.printStackTrace();
             return false;
@@ -100,19 +94,28 @@ public class KeyFileUtilWithJKS implements I_KeyFileUtil {
 
     @Override
     public PrivateKey getPrivateKeyFromFile() throws Exception {
-        KeyStore keyStore = KeyStore.getInstance(KEYSTORE_TYPE);
-        keyStore.load(new FileInputStream(KEYSTORE_FILEPATH), KEYSTORE_PASSWORD.toCharArray());
 
-        return (PrivateKey) keyStore.getKey(PRIVKEY_ALIAS, KEYSTORE_PASSWORD.toCharArray());
+        String KEYSTORE_FILEPATH = getFilePath(KEYSTORE_FILENAME);
+
+        KeyStore keyStore = KeyStore.getInstance(KEYSTORE_TYPE);
+        FileInputStream fis = new FileInputStream(KEYSTORE_FILEPATH);
+        keyStore.load(fis, KEYSTORE_PASSWORD.toCharArray());
+        PrivateKey privateKey = (PrivateKey) keyStore.getKey(KEYSTORE_ALIAS, KEYSTORE_PASSWORD.toCharArray());
+        fis.close();
+        return privateKey;
     }
 
     @Override
     public PublicKey getPublicKeyFromFile() throws Exception {
-        KeyStore keyStore = KeyStore.getInstance(KEYSTORE_TYPE);
-        keyStore.load(new FileInputStream(KEYSTORE_FILEPATH), KEYSTORE_PASSWORD.toCharArray());
 
-        Certificate cert = keyStore.getCertificate();
-        return cert.getPublicKey();
+        String CERT_FILEPATH =  getFilePath(CERT_FILENAME);
+
+        FileInputStream fis = new FileInputStream(CERT_FILEPATH);
+        CertificateFactory cf = CertificateFactory.getInstance(CERTIFICATE_TYPE);
+        X509Certificate certificate = (X509Certificate) cf.generateCertificate(fis);
+        PublicKey pubKey = certificate.getPublicKey();
+        fis.close();
+        return pubKey;
     }
 
 }
