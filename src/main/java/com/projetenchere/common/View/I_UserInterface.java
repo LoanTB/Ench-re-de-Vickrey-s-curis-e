add7b0c3a393ef6e511fe97e40afe55745ff1750
auxilienk <auxilien@etud.fr>
2023-12-17 17:43:22 +0100

    <feat> [Sprint3] : Ajout de I_UserInterface.java

---
+++ src/main/java/com/projetenchere/common/View/I_UserInterface.java
@@ -0,0 +1,10 @@
+package com.projetenchere.common.View;
+
+public interface I_UserInterface {
+    void tellSignatureConfigSetup();
+
+    void tellSignatureConfigGeneration();
+
+    void tellSignatureConfigReady();
+    //TODO :
+}
