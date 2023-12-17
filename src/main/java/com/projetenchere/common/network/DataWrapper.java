aa3c2548da2e0bd16f9b82fd8278261d76b30cc6
Paul Jacquemin <paul.jacquemin@umontpellier.fr>
2023-12-17 12:45:24 +0100

    (WIP) refactor: fusionné le comportement de NetworkData dans DataWrapper

--- src/main/java/com/projetenchere/common/network/DataWrapper.java
+++ src/main/java/com/projetenchere/common/network/DataWrapper.java
@@ -5,16 +5,43 @@ import org.jetbrains.annotations.Nullable;
 import java.io.Serializable;
 import java.security.Signature;
 
-public class DataWrapper<T extends Serializable> extends NetworkData {
+public class DataWrapper<T extends Serializable> {
+    @Nullable
     private final T object;
 
-    public DataWrapper(T object, Headers header, @Nullable Signature signature) {
-        super(header, signature);
+    private Headers header;
+
+    public DataWrapper(T object, Headers header) {
         this.object = object;
+        this.header = header;
+    }
+
+    public DataWrapper(Headers header) {
+        this(null, header);
+    }
+
+    public boolean containsWrappedObject() {
+        return this.object != null;
+    }
+    
+    public Headers getHeader() {
+        return this.header;
+    }
+
+    @SuppressWarnings("BooleanMethodIsAlwaysInverted")
+    public boolean checkHeader(Headers otherHeader) {
+        if (this.getHeader().equals(Headers.ERROR) || otherHeader.equals(Headers.ERROR)) {
+            return false;
+        }
+        return this.getHeader().equals(otherHeader);
+    }
+
+    public static DataWrapper<?> error() {
+        return new DataWrapper<>(Headers.ERROR, null);
     }
 
     public T unwrap() {
-        return this.object;
+        return containsWrappedObject() ? this.object : null;
     }
 
 }
