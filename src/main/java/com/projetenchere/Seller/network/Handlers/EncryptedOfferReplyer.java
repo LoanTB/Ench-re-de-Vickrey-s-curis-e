2a4a385895a0c77c70d36e00baa86447c57315ce
auxilienk <auxilien@etud.fr>
2023-12-17 18:33:16 +0100

    <feat> [Sprint3] : Vérification de la Signature

--- src/main/java/com/projetenchere/Seller/network/Handlers/EncryptedOfferReplyer.java
+++ src/main/java/com/projetenchere/Seller/network/Handlers/EncryptedOfferReplyer.java
@@ -3,11 +3,13 @@ package com.projetenchere.Seller.network.Handlers;
 import com.projetenchere.Seller.Model.Seller;
 import com.projetenchere.common.Models.Encrypted.EncryptedOffer;
 import com.projetenchere.common.Models.WinStatus;
+import com.projetenchere.common.Utils.SignatureUtil;
 import com.projetenchere.common.network.DataWrapper;
 import com.projetenchere.common.network.IDataHandler;
 import com.projetenchere.common.network.Headers;
 
 import java.io.Serializable;
+import java.security.SignatureException;
 
 public class EncryptedOfferReplyer implements IDataHandler {
 
@@ -17,16 +19,21 @@ public class EncryptedOfferReplyer implements IDataHandler {
         if (!seller.resultsAreIn()) {
             try {
                 EncryptedOffer offer = (EncryptedOffer) data;
-                seller.addBidder(offer.getSignature(), offer.getPrice());
+
+                SignatureUtil.verifyDataSignature(offer.getPrice(),offer.getPriceSigned(),offer.getPublicKey());
+                seller.addBidder(offer.getPublicKey(), offer.getPrice());
+
                 while (!seller.resultsAreIn()) {
                     wait(1000);
                 }
-                WinStatus status = seller.getSignatureWinStatus(offer.getSignature());
+                WinStatus status = seller.getSignatureWinStatus(offer.getPublicKey());
                 return new DataWrapper<>(status, Headers.OK_WIN_STATUS);
             } catch (ClassCastException e) {
                 throw new RuntimeException("Received unreadable data");
             } catch (InterruptedException e) {
                 throw new RuntimeException("Timeout");
+            } catch (SignatureException e) {
+                throw new RuntimeException("Signature falsify");
             }
         } else return new DataWrapper<>(null, Headers.ERROR);
     }
