454709175d8966f40fc48a230418214d830069cd
Yûki <kilakiyu+java@gmail.com>
2023-12-31 00:58:15 +0100

    fix: merge de master à GraphicInterface

--- src/main/java/com/projetenchere/Manager/View/graphicalUserInterface/ManagerGraphicalUserInterface.java
+++ src/main/java/com/projetenchere/Manager/View/graphicalUserInterface/ManagerGraphicalUserInterface.java
@@ -1,8 +1,8 @@
 package com.projetenchere.Manager.View.graphicalUserInterface;
 
 import com.projetenchere.Manager.View.IManagerUserInterface;
-import com.projetenchere.common.Model.Bid;
-import com.projetenchere.common.Model.Winner;
+import com.projetenchere.common.Models.Bid;
+import com.projetenchere.common.Models.Winner;
 
 import java.time.LocalDateTime;
 
@@ -10,8 +10,53 @@ public class ManagerGraphicalUserInterface implements IManagerUserInterface {
 
 
     @Override
-    public Bid askBidInformations() {
-        return null;
+    public void displayHello() {
+
+    }
+
+    @Override
+    public void tellSignatureConfigSetup() {
+
+    }
+
+    @Override
+    public void tellSignatureConfigGeneration() {
+
+    }
+
+    @Override
+    public void tellSignatureConfigReady() {
+
+    }
+
+    @Override
+    public void displayGenerateKey() {
+
+    }
+
+    @Override
+    public void displayBidLaunch() {
+
+    }
+
+    @Override
+    public void displayReceivedPrices() {
+
+    }
+
+    @Override
+    public void displayPriceProcessing() {
+
+    }
+
+    @Override
+    public void displaySentWinnerPrice() {
+
+    }
+
+    @Override
+    public void displayEndOfAuction() {
+
     }
 
     @Override
@@ -36,6 +81,46 @@ public class ManagerGraphicalUserInterface implements IManagerUserInterface {
 
     @Override
     public void displayWinnerPrice(Winner winner) {
+
+    }
+
+    @Override
+    public void tellConnectingNewSeller(String id) {
+
+    }
+
+    @Override
+    public void tellConnectingNewBidder(String id) {
+
+    }
+
+    @Override
+    public void tellRequestInformationAboutBiddersBySeller(String id) {
+
+    }
+
+    @Override
+    public void tellRequestCurrentBidsByBidder(String id) {
+
+    }
+
+    @Override
+    public void tellRequestToDetermineTheWinnerOfBidBySeller(String idBid, String idSeller) {
+
+    }
+
+    @Override
+    public void tellManagerReadyToProcessBids() {
+
+    }
+
+    @Override
+    public void tellBidReceivedby(String idSeller, String idBid) {
+
+    }
+
+    @Override
+    public void tellReceivingAndReplyToInformationOf(String id, String type) {
 
     }
 }
