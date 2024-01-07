e635688060c23416d457e32a38d87dda5a122ccf
Yûki <kilakiyu+java@gmail.com>
2024-01-07 05:41:18 +0100

    refactor: on peut launch depuis SellerApp.java

--- src/main/java/com/projetenchere/Seller/SellerApp.java
+++ src/main/java/com/projetenchere/Seller/SellerApp.java
@@ -1,20 +1,10 @@
 package com.projetenchere.Seller;
 
-import com.projetenchere.Seller.Controllers.SellerController;
-import com.projetenchere.Seller.View.graphicalUserInterface.ISellerUserInterfaceFactory;
-import com.projetenchere.Seller.View.graphicalUserInterface.SellerUserInterfaceFactory;
+import com.projetenchere.Seller.View.graphicalUserInterface.SellerAppLoader;
+import javafx.application.Application;
 
 public class SellerApp {
-    public static void main(String[] args) throws Exception {
-        ISellerUserInterfaceFactory uiFactory = new SellerUserInterfaceFactory();
-        SellerController controller = new SellerController(uiFactory);
-        controller.displayHello();
-        controller.setSignatureConfig();
-        controller.createMyBid();
-        controller.sendMyBid();
-        controller.receiveOffersUntilBidEndAndSendResults();
-        controller.sendEncryptedOffersSet();
-        controller.displayWinner();
+    public static void main(String[] args) {
+        Application.launch(SellerAppLoader.class, args);
     }
 }
-
--- src/main/java/com/projetenchere/Seller/View/graphicalUserInterface/SellerAppLoader.java
+++ src/main/java/com/projetenchere/Seller/View/graphicalUserInterface/SellerAppLoader.java
@@ -5,21 +5,23 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import com.projetenchere.Seller.Controllers.SellerController;
 
 public class SellerAppLoader extends Application {
 
+    private SellerController controller;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/SellerGraphicalUserInterface.fxml"));
 
         ISellerUserInterfaceFactory uiFactory = new SellerUserInterfaceFactory();
-        SellerGraphicalUserInterface guiInterface = (SellerGraphicalUserInterface) uiFactory.createSellerUserInterface();
 
-        // Load FXML and apply controler
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/SellerGraphicalUserInterface.fxml"));
+        SellerGraphicalUserInterface guiInterface = (SellerGraphicalUserInterface) uiFactory.createSellerUserInterface();
         loader.setController(guiInterface);
+
         Parent root = loader.load();
 
-        // Create scene and add CSS
         Scene scene = new Scene(root);
         String css = this.getClass().getResource("/css/style.css").toExternalForm();
         scene.getStylesheets().add(css);
@@ -27,6 +29,16 @@ public class SellerAppLoader extends Application {
         primaryStage.setTitle("Seller");
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        this.controller = new SellerController(uiFactory);
+
+        this.controller.displayHello();
+        this.controller.setSignatureConfig();
+        this.controller.createMyBid();
+        this.controller.sendMyBid();
+        this.controller.receiveOffersUntilBidEndAndSendResults();
+        this.controller.sendEncryptedOffersSet();
+        this.controller.displayWinner();
     }
 
     public static void main(String[] args) {
