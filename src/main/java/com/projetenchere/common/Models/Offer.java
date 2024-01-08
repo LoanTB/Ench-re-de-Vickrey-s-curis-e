58f6ba05b92306d4717279c6436483d1f9302b62
auxilienk <auxilien@etud.fr>
2024-01-08 22:25:44 +0100

    <fix> [Sprint4] : Suppression de l'attribut Signature contenu dans offer qui n'est pas utilisé.

--- src/main/java/com/projetenchere/common/Models/Offer.java
+++ src/main/java/com/projetenchere/common/Models/Offer.java
@@ -4,19 +4,20 @@ import java.io.Serializable;
 import java.security.Signature;
 
 public class Offer implements Serializable {
-    private final Signature signature;
+    //private final Signature signature;
     private final String idBid;
     private final double price;
 
-    public Offer(Signature signature, String idBid, String price){
-        this.signature = signature;
+    public Offer(String idBid, String price){
+//        this.signature = signature;
         this.idBid = idBid;
         this.price = Double.parseDouble(price);
     }
 
+    /*
     public Signature getSignature() {
         return signature;
-    }
+    }*/
 
     public String getIdBid() {
         return idBid;
