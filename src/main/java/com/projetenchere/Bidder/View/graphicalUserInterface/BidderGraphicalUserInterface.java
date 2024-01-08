46cabbdd6b5622430f8a6ca6779aa180e7abb0cf
yuuki <yuki.sala-mochizuki@etu.umontpellier.fr>
2024-01-08 02:15:16 +0100

    feat: Fonctions pour afficher les messages de Bidder.

--- src/main/java/com/projetenchere/Bidder/View/graphicalUserInterface/BidderGraphicalUserInterface.java
+++ src/main/java/com/projetenchere/Bidder/View/graphicalUserInterface/BidderGraphicalUserInterface.java
@@ -31,7 +31,8 @@ public class BidderGraphicalUserInterface implements IBidderUserInterface {
 
     @Override
     public void displayBid(CurrentBids currentBids) {
-
+        addLogMessage("Enchères Actuelle :");
+        addLogMessage(currentBids.toString()+"\n");
     }
 
     @Override
@@ -40,48 +41,46 @@ public class BidderGraphicalUserInterface implements IBidderUserInterface {
     }
 
     @Override
-    public void tellSignatureConfigSetup() {
-
+    public void tellSignatureConfigSetup(){
+        addLogMessage("Mise en place de la configuration de la signature...");
     }
-
     @Override
-    public void tellSignatureConfigGeneration() {
-
+    public void tellSignatureConfigGeneration(){
+        addLogMessage("Génération de la configuration de la signature ...");
     }
-
     @Override
-    public void tellSignatureConfigReady() {
-
+    public void tellSignatureConfigReady(){
+        addLogMessage("Configuration de la signature terminée.");
     }
 
     @Override
     public void tellOfferWon(double priceToPay) {
-
+        addLogMessage("Votre offre a gagné, vous devez payer " + priceToPay + "€");
     }
 
     @Override
     public void tellOfferLost() {
-
+        addLogMessage("Votre offre a perdu");
     }
 
     @Override
     public void tellOfferSent() {
-
+        addLogMessage("Votre offre a bien été envoyé");
     }
 
     @Override
     public void tellWaitOfferResult() {
-
+        addLogMessage("Attente des résultats...");
     }
 
     @Override
     public void tellWaitBidsAnnoncement() {
-
+        addLogMessage("Attente de reception des enchères en cours...");
     }
 
     @Override
     public void tellWaitBidsPublicKeysAnnoncement() {
-
+        addLogMessage("Attente/Verification de reception des clés des enchères en cours...");
     }
 
     @Override
@@ -111,36 +110,36 @@ public class BidderGraphicalUserInterface implements IBidderUserInterface {
 
     @Override
     public void tellWaitManagerSecurityInformations() {
-
+        addLogMessage("Attente des informations de sécurité du gestionnaire...");
     }
 
     @Override
     public void displayHello() {
-
+        addLogMessage("Bienvenue Enchérisseur !");
     }
 
     @Override
     public void tellReceivingInformationOf(String id, String type) {
-
+        addLogMessage("Reception d'information de "+type+" "+id);
     }
 
     @Override
     public void tellReceiptOfCurrentBids() {
-
+        addLogMessage("Reception des enchères actuelles");
     }
 
     @Override
     public void tellReceiptOfEncryptionKeysForCurrentBids() {
-
+        addLogMessage("Reception des clés de chiffrement des enchères actuelles");
     }
 
     @Override
     public void tellReceiptOfBidResult(String id) {
-
+        addLogMessage("Reception des résultats de l'enchère "+id);
     }
 
     @Override
     public void tellSendRequestOffers() {
-
+        addLogMessage("Envoie de la demande d'enchères actuelles au gestionnaire");
     }
 }
