712bd35c6eb63dedf04834a8460d20a8ffbb1975
Paul <paul.jacquemin@etu.umontpellier.fr>
2024-01-08 16:05:50 +0100

    refactor: précisé certaines exceptions

--- src/main/java/com/projetenchere/common/Models/Encrypted/EncryptedOffer.java
+++ src/main/java/com/projetenchere/common/Models/Encrypted/EncryptedOffer.java
@@ -5,6 +5,7 @@ import com.projetenchere.common.Utils.EncryptionUtil;
 import com.projetenchere.common.Utils.SignatureUtil;
 
 import java.io.Serializable;
+import java.security.GeneralSecurityException;
 import java.security.PublicKey;
 import java.security.Signature;
 
@@ -14,7 +15,7 @@ public class EncryptedOffer implements Serializable {
     private PublicKey SignaturePubKey;
     private final byte[] price;
 
-    public EncryptedOffer(Signature signature, Offer offer, PublicKey SignaturePubKey, PublicKey managerPubKey, String bidId) throws Exception {
+    public EncryptedOffer(Signature signature, Offer offer, PublicKey SignaturePubKey, PublicKey managerPubKey, String bidId) throws GeneralSecurityException {
         this.price = EncryptionUtil.encryptPrice(offer.getPrice(), managerPubKey);
         this.priceSigned = SignatureUtil.signData(price,signature);
         this.SignaturePubKey = SignaturePubKey;
