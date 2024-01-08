ad4ed5c2b54bf7d5f50cc6247368f8f895993837
auxilienk <auxilien@etud.fr>
2024-01-08 16:53:12 +0100

    <feat> [Sprint4] : Dev de SignedEncryptedOfferSet

--- src/main/java/com/projetenchere/common/Models/Encrypted/SignedEncryptedOfferSet.java
+++ src/main/java/com/projetenchere/common/Models/Encrypted/SignedEncryptedOfferSet.java
@@ -1,4 +1,32 @@
 package com.projetenchere.common.Models.Encrypted;
+import com.projetenchere.common.Utils.SignatureUtil;
+import java.io.Serializable;
+import java.security.GeneralSecurityException;
+import java.security.PublicKey;
+import java.security.Signature;
 
-public class SignedEncryptedOfferSet {
+public class SignedEncryptedOfferSet implements Serializable {
+
+    private final byte[] setSigned;
+    private final EncryptedOffersSet set;
+    private PublicKey signaturePubKey;
+
+    public SignedEncryptedOfferSet(Signature signature, PublicKey signaturePubKey, EncryptedOffersSet list) throws GeneralSecurityException {
+        this.set = list;
+        this.setSigned = SignatureUtil.signData(list,signature);
+        this.signaturePubKey = signaturePubKey;
+    }
+
+    public byte[] getSetSigned() {
+        return setSigned;
+    }
+
+    public PublicKey getSignaturePubKey() {
+        return signaturePubKey;
+    }
+
+    public EncryptedOffersSet getSet() {
+        return set;
+    }
+
 }
