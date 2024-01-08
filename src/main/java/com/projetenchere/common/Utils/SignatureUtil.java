9bdbf85ebf38db475a8f28ebc8990479170b4b1c
auxilienk <auxilien@etud.fr>
2024-01-08 16:52:35 +0100

    <feat> [Sprint4] : Ajout de methodes en surchage qui prend en argument EncryptedOffersSet pour faire la conversion en byte[].

--- src/main/java/com/projetenchere/common/Utils/SignatureUtil.java
+++ src/main/java/com/projetenchere/common/Utils/SignatureUtil.java
@@ -1,5 +1,9 @@
 package com.projetenchere.common.Utils;
 
+import com.projetenchere.common.Models.Encrypted.EncryptedOffersSet;
+import com.projetenchere.common.Models.Encrypted.SignedEncryptedOfferSet;
+
+import java.io.*;
 import java.security.Signature;
 import java.security.PublicKey;
 import java.security.PrivateKey;
@@ -25,11 +29,53 @@ public class SignatureUtil {
         return signature.sign();
     }
 
+    public static byte[] signData(EncryptedOffersSet data, Signature signature) throws SignatureException {
+        return signData(objectToArrayByte(data),signature);
+    }
+
+    public static byte[] objectToArrayByte(EncryptedOffersSet objetSerializable){
+        byte[] tab = new byte[0];
+        try{
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(baos);
+            oos.writeObject(objetSerializable);
+            tab= baos.toByteArray();
+            oos.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return tab;
+    }
+
+    public static EncryptedOffersSet toObject(byte[] tab){
+        EncryptedOffersSet objet = null;
+        try{
+            ByteArrayInputStream bais = new ByteArrayInputStream(tab);
+            ObjectInputStream ois = new ObjectInputStream(bais);
+            objet = (EncryptedOffersSet) ois.readObject();
+            ois.close();
+        }catch (IOException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        return objet;
+    }
+
     public static boolean verifyDataSignature(byte[] data, byte[] signatureBytes, PublicKey publicKey) throws SignatureException {
         try {
             Signature signature = Signature.getInstance(SIGNATURE_ALGORITHM);
             signature.initVerify(publicKey);
             signature.update(data);
+            return signature.verify(signatureBytes);
+        } catch (NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new SignatureException(e);
+        }
+    }
+
+    public static boolean verifyDataSignature(EncryptedOffersSet data, byte[] signatureBytes, PublicKey publicKey) throws SignatureException {
+        try {
+            Signature signature = Signature.getInstance(SIGNATURE_ALGORITHM);
+            signature.initVerify(publicKey);
+            signature.update(objectToArrayByte(data));
             return signature.verify(signatureBytes);
         } catch (NoSuchAlgorithmException | InvalidKeyException e) {
             throw new SignatureException(e);
