ee4fbd1ac3782883d9f226ffb590834c5db0cde6
Loan Tremoulet Breton <loan.tremoulet-breton01@etu.umontpellier.fr>
2024-01-11 20:55:30 +0100

    feat: Ajout d'affichages suplémentaires

--- src/main/java/com/projetenchere/Manager/Handlers/NewBidReplyer.java
+++ src/main/java/com/projetenchere/Manager/Handlers/NewBidReplyer.java
@@ -13,7 +13,7 @@ public class NewBidReplyer implements IDataHandler {
     @Override
     public <T extends Serializable> DataWrapper<T> handle(Serializable data) {
         Manager.getInstance().addBid((Bid) data);
-        ManagerGraphicalUserInterface.getInstance().addLogMessage("Nouvelle enchère reçue : "+((Bid) data).getName());
+        ManagerGraphicalUserInterface.getInstance().addLogMessage("Nouvelle enchère reçue : "+((Bid) data).getName()+" (+"+((Bid) data).getId()+"+)");
         return new DataWrapper<>(Headers.OK_NEW_BID);
     }
 }
