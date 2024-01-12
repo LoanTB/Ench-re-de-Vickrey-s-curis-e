04ff7c36db804c82c2a4eef2104fe125a3ee328d
Auxilien Katia <auxilien@etud.fr>
2024-01-12 01:09:54 +0000

    Update CertificatSSLFile.java

--- src/main/java/com/projetenchere/common/Utils/CertificatSSLFile.java
+++ src/main/java/com/projetenchere/common/Utils/CertificatSSLFile.java
@@ -1,57 +1,31 @@
 package com.projetenchere.common.Utils;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
-import java.security.KeyStore;
 
 public class CertificatSSLFile {
 
-    /*public static void main(String[] args) {
+    public static void main(String[] args) {
         // Spécifiez le chemin du fichier source et de destination
         String sourcePath = "/chemin/vers/source.jks";
         String destinationPath = "/chemin/vers/destination.jks";
 
         try {
-            // Méthode 1 : Utiliser les API Java standard (Java 7 et versions ultérieures)
-            copyJksUsingJava7NIO(sourcePath, destinationPath);
-
-            // Méthode 2 : Utiliser les flux d'entrée/sortie
-            // copyJksUsingStreams(sourcePath, destinationPath);
-        } catch (IOException e) {
+            copyJks(sourcePath, destinationPath);
+            System.out.println("Copie réussie de " + sourcePath + " vers " + destinationPath);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-*/
 
-    private static void copyJksUsingJava7NIO(String sourcePath, String destinationPath) throws IOException {
+    private static void copyJks(String sourcePath, String destinationPath) throws Exception {
         Path source = Path.of(sourcePath);
         Path destination = Path.of(destinationPath);
 
-        // Utilisez Files.copy pour copier le fichier (Java 7 et versions ultérieures)
-        Files.copy(source, destination, StandardCopyOption.REPLACE_EXISTING);
+        // Utilisez Files.copy pour copier le fichier avec ARM (Automatic Resource Management)
+        try (var inputStream = Files.newInputStream(source);
+             var outputStream = Files.newOutputStream(destination)) {
 
-        System.out.println("Copie réussie de " + sourcePath + " vers " + destinationPath);
-    }
-
-    private static void copyJksUsingStreams(String sourcePath, String destinationPath) throws IOException {
-        try (InputStream inputStream = new FileInputStream(sourcePath);
-             OutputStream outputStream = new FileOutputStream(destinationPath)) {
-
-            // Créez un tampon pour stocker les données pendant la copie
-            byte[] buffer = new byte[1024];
-            int bytesRead;
-
-            // Lire à partir du fichier source et écrire dans le fichier de destination
-            while ((bytesRead = inputStream.read(buffer)) != -1) {
-                outputStream.write(buffer, 0, bytesRead);
-            }
-
-            System.out.println("Copie réussie de " + sourcePath + " vers " + destinationPath);
+            Files.copy(inputStream, destination, StandardCopyOption.REPLACE_EXISTING);
         }
     }
 }
