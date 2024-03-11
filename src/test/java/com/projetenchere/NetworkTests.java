e685e46e4d97a116cc456d1e7378f3d5aaf1fa28
Loan Tremoulet Breton <loan.tremoulet-breton01@etu.umontpellier.fr>
2024-03-11 17:09:36 +0100

    feat: Initialisation des tests de network

--- src/test/java/com/projetenchere/NetworkTests.java
+++ src/test/java/com/projetenchere/NetworkTests.java
@@ -1,4 +1,14 @@
 package com.projetenchere;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+
 public class NetworkTests {
+    @Nested
+    @DisplayName("Connexion")
+    public class TestConnexion {}
+
+    @Nested
+    @DisplayName("Usage")
+    public class TestUsage {}
 }
