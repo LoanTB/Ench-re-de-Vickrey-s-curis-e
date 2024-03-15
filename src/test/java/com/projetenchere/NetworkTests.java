a4c93a21f0d77e85ac0640a2299520ae3a642307
Loan Tremoulet Breton <loan.tremoulet-breton01@etu.umontpellier.fr>
2024-03-15 10:15:50 +0100

    feat: Mise en place des tests principaux

--- src/test/java/com/projetenchere/NetworkTests.java
+++ src/test/java/com/projetenchere/NetworkTests.java
@@ -2,11 +2,12 @@ package com.projetenchere;
 
 import com.projetenchere.common.network.*;
 import com.projetenchere.common.network.socket.SSLSocketFactory;
+import org.jetbrains.annotations.Blocking;
 import org.junit.jupiter.api.*;
 
 import java.io.Serializable;
 import java.net.InetSocketAddress;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 import static java.lang.Thread.sleep;
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,73 +17,49 @@ public class NetworkTests {
     private static Client client;
     private static ClientSocketWrapper clientSocketWrapper;
 
-    @BeforeAll
-    static void setupTest() {
-        server = new Server(2048);
-        client = new Client();
-    }
-
-    @AfterEach
-    void closeClient() {
-        if (clientSocketWrapper != null) {
-            //Close socket client
-        }
+    @AfterAll
+    static void closeConnexion() {
+        server.stopAllConnections();
+        clientSocketWrapper.close();
     }
 
     @Test
-    @Order(0)
-    @Timeout(value = 250, unit = TimeUnit.MILLISECONDS)
-    void TestStartServer() throws InterruptedException {
+    @BeforeAll
+    @Blocking
+    @Timeout(value = 500, unit = TimeUnit.MILLISECONDS)
+    static void TestStartServerAndClient() throws InterruptedException {
+        server = new Server(2048);
+        client = new Client();
+
         server.addHandler(Headers.TEST, new IDataHandler() {
             @Override
             public DataWrapper<String> handle(Serializable data) {
                 return new DataWrapper<String>("OK", Headers.TEST);
             }
         });
+
+
         server.addHandler(Headers.OK_TEST, new IDataHandler() {
             @Override
             public DataWrapper<StringBuilder> handle(Serializable data) {
                 return new DataWrapper<StringBuilder>(new StringBuilder("C'est bien vrais ça"), Headers.TEST);
             }
         });
+
         server.start();
-        while(!server.isReady()) {sleep(10, 0);}
+        while(!server.isReady()){sleep(10, 0);}
+
         clientSocketWrapper = new ClientSocketWrapper(
                 (new SSLSocketFactory()).createSocket(
                         new InetSocketAddress("127.0.0.100", 2048)
                 )
         );
+        while(!clientSocketWrapper.isConnected()){sleep(10, 0);}
     }
 
     @Test
     @Order(1)
-    @Timeout(value = 1)
-    void TestSendToBadAddress() {
-        assertThrows(RuntimeException.class, () -> {
-            client.fetch(
-                    clientSocketWrapper,
-                    Headers.TEST,
-                    Headers.TEST
-            );
-        });
-    }
-
-    @Test
-    @Order(1)
-    @Timeout(value = 1)
-    void TestSendToBadPort() {
-        assertThrows(RuntimeException.class, () -> {
-            client.fetch(
-                    clientSocketWrapper,
-                    Headers.TEST,
-                    Headers.TEST
-            );
-        });
-    }
-
-    @Test
-    @Order(1)
-    @Timeout(value = 1)
+    @Timeout(value = 200, unit = TimeUnit.MILLISECONDS)
     void TestSendAndReceiveData() {
         String test = client.fetch(
                 clientSocketWrapper,
@@ -90,12 +67,11 @@ public class NetworkTests {
                 Headers.TEST
         );
         assertEquals("OK", test);
-
     }
 
     @Test
-    @Order(1)
-    @Timeout(value = 1)
+    @Order(2)
+    @Timeout(value = 200, unit = TimeUnit.MILLISECONDS)
     void TestRequestBadHeader() {
         assertThrows(RuntimeException.class, () -> {
             client.fetch(
@@ -108,28 +84,7 @@ public class NetworkTests {
 
     @Test
     @Order(1)
-    @Timeout(value = 1)
-    void TestSendBadHeader() {
-        assertThrows(RuntimeException.class, () -> {
-            client.fetch(
-                    clientSocketWrapper,
-                    Headers.ERROR,
-                    Headers.TEST
-            );
-        });
-
-        assertThrows(RuntimeException.class, () -> {
-            client.fetch(
-                    clientSocketWrapper,
-                    Headers.OK_TEST,
-                    Headers.OK_TEST
-            );
-        });
-    }
-
-    @Test
-    @Order(1)
-    @Timeout(value = 1)
+    @Timeout(value = 200, unit = TimeUnit.MILLISECONDS)
     void TestSendAndRecieveDataWithOtherHeader() {
         StringBuilder test = client.fetch(
                 clientSocketWrapper,
