c48307b2d45aea3fddfaf5f355e43f358111daaa
K. Auxilien <125131297+KatiaAuxilien@users.noreply.github.com>
2024-03-16 17:43:49 +0100

    <refactor> Creation d'une classe abstraite AbstractSignedPack

---
+++ src/main/java/com/projetenchere/common/Models/Encrypted/AbstractSignedPack.java
@@ -0,0 +1,46 @@
+package com.projetenchere.common.Models.Encrypted;
+
+import java.security.PublicKey;
+
+//TODO : JavaDoc
+public abstract class AbstractSignedPack {
+    protected final Object object;
+    protected final byte[] objectSigned;
+    protected final PublicKey signaturePubKey;
+
+    /**
+     *
+     * @param object
+     * @param objectSigned
+     * @param signaturePubKey
+     */
+    protected AbstractSignedPack(Object object, byte[] objectSigned, PublicKey signaturePubKey) {
+        this.object = object;
+        this.objectSigned = objectSigned;
+        this.signaturePubKey = signaturePubKey;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public Object getObject() {
+        return object;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public byte[] getObjectSigned() {
+        return objectSigned;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public PublicKey getSignaturePubKey() {
+        return signaturePubKey;
+    }
+}
