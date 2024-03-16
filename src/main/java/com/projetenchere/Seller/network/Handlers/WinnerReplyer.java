2963e4f0ed7d2d305b8b15d9cf29664a051df92d
K. Auxilien <125131297+KatiaAuxilien@users.noreply.github.com>
2024-03-16 17:54:18 +0100

    <feat : nouveauProtocole> Ajout d'un nouveau Handler pour plus tard : Dépend de l'implémentation du nouveau protocole auprès des enchérisseurs.

---
+++ src/main/java/com/projetenchere/Seller/network/Handlers/WinnerReplyer.java
@@ -0,0 +1,56 @@
+
+/*
+package com.projetenchere.Seller.network.Handlers;
+
+import com.projetenchere.Seller.Model.Seller;
+import com.projetenchere.common.network.DataWrapper;
+import com.projetenchere.common.network.Headers;
+import com.projetenchere.common.network.IDataHandler;
+
+import java.io.Serializable;
+
+public class WinnerReplyer implements IDataHandler {
+    @Override
+    public DataWrapper<??> handle(Serializable data) {
+        Seller seller = Seller.getInstance();
+        synchronized (this) {
+
+            try {
+                SignedPublicKey signedPublicKey = (SignedPublicKey) data;
+                PublicKey bidderPk = null;
+                WinStatus status;
+
+
+                if (SignatureUtil.verifyDataSignature("ok".getBytes(), signedPublicKey.getOkSigned(), signedPublicKey.getPublicKey())) {
+                    Set<EncryptedOffer> offers = seller.getEncryptedOffersSet().getOffers();
+
+                    for (EncryptedOffer offer : offers) {
+                        if (offer.getSignaturePublicKey() == signedPublicKey.getPublicKey()) {
+                            bidderPk = signedPublicKey.getPublicKey();
+                            seller.getbiddersOk().add(bidderPk);
+                        }
+                    }
+                }
+
+                while (!seller.resultsAreIn()) {
+                    wait(1000);
+                }
+
+                if (bidderPk == null) {
+                    status = new WinStatus(seller.getMyBid().getId(), false, 0);
+                } else {
+                    status = seller.getSignatureWinStatus(bidderPk);
+                }
+
+                return new DataWrapper<>(status, Headers.OK_WIN_STATUS);
+            } catch (ClassCastException e) {
+                throw new RuntimeException("Received unreadable data");
+            } catch (InterruptedException e) {
+                throw new RuntimeException("Timeout");
+            } catch (SignatureException e) {
+                throw new RuntimeException("Signature falsify");
+            }
+        }
+    }
+}
+ */
