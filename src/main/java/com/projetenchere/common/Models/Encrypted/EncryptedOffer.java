b2af2dc08fb084be2646e8b6aaa0e19b86a471a5
K. Auxilien <125131297+KatiaAuxilien@users.noreply.github.com>
2024-03-16 18:11:14 +0100

    <refactor> EncryptedOffer hérite d'Abstract Signed Pack et changement de l'implémentation du constructeur

--- src/main/java/com/projetenchere/common/Models/Encrypted/EncryptedOffer.java
+++ src/main/java/com/projetenchere/common/Models/Encrypted/EncryptedOffer.java
@@ -1,48 +1,24 @@
 package com.projetenchere.common.Models.Encrypted;
 
-import com.projetenchere.common.Models.Offer;
-import com.projetenchere.common.Utils.EncryptionUtil;
-import com.projetenchere.common.Utils.SignatureUtil;
-
 import java.io.Serializable;
-import java.security.GeneralSecurityException;
 import java.security.PublicKey;
-import java.security.Signature;
 
-public class EncryptedOffer implements Serializable {
+//TODO : JavaDoc
+public class EncryptedOffer extends AbstractSignedPack implements Serializable {
     private final String bidId;
-    private final byte[] price;
-    private final byte[] priceSigned;
-    private final PublicKey SignaturePubKey;
 
-    public EncryptedOffer(Signature signature, Offer offer, PublicKey SignaturePubKey, PublicKey managerPubKey, String bidId) throws GeneralSecurityException {
-        this.price = EncryptionUtil.encryptPrice(offer.getPrice(), managerPubKey);
-        this.priceSigned = SignatureUtil.signData(price, signature);
-        this.SignaturePubKey = SignaturePubKey;
+    /**
+     * @param encPrice
+     * @param encPriceSigned
+     * @param bidderSignaturePubKey
+     * @param bidId
+     */
+    public EncryptedOffer(byte[] encPrice, byte[] encPriceSigned, PublicKey bidderSignaturePubKey, String bidId) {
+        super(encPrice, encPriceSigned, bidderSignaturePubKey);
         this.bidId = bidId;
-    }
-
-    public EncryptedOffer(Signature signature, byte[] price, PublicKey SignaturePubKey, String bidId) throws Exception {
-        this.price = price;
-        this.priceSigned = SignatureUtil.signData(price, signature);
-        this.SignaturePubKey = SignaturePubKey;
-        this.bidId = bidId;
-    }
-
-    public byte[] getPriceSigned() {
-        return this.priceSigned;
     }
 
     public String getBidId() {
         return bidId;
     }
-
-    public byte[] getPrice() {
-        return price;
-    }
-
-    public PublicKey getSignaturePublicKey() {
-        return SignaturePubKey;
-    }
-
 }
