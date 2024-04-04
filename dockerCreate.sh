8adb5ea811dcc1062eb1e8b336930f6add79a52d
Vachalde <remi.vachalde@etu.umontpellier.fr>
2024-04-04 12:09:18 +0200

    Add: ajout du fichier dockerCreate.sh

---
+++ dockerCreate.sh
@@ -0,0 +1,6 @@
+./compile.sh
+docker login -u remiti -p 'H&bb6p&)YjXwi]*'
+docker build -t remiti/securewin_manager:latest .
+docker tag remiti/securewin_manager:latest remiti/securewin_manager
+docker push remiti/securewin_manager
+#docker rmi remiti/securewin_manager
