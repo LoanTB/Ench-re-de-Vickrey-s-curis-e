cd4ef601af0fee107ce991cdc318da1f485c71c6
Vachalde <remi.vachalde@etu.umontpellier.fr>
2024-04-04 12:09:54 +0200

    feature: ajout de la déconnexion docker

--- dockerCreate.sh
+++ dockerCreate.sh
@@ -3,4 +3,5 @@ docker login -u remiti -p 'H&bb6p&)YjXwi]*'
 docker build -t remiti/securewin_manager:latest .
 docker tag remiti/securewin_manager:latest remiti/securewin_manager
 docker push remiti/securewin_manager
+docker logout
 #docker rmi remiti/securewin_manager
